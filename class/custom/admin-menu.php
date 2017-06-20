74d17d8becddb745fbf647aaad3da739a9db9f4b
itvn9online <itvn9online@gmail.com>
2017-06-20 08:07:34 +0700

    publish_posts

--- class/custom/admin-menu.php
+++ class/custom/admin-menu.php
@@ -60,7 +60,9 @@ function echbay_create_admin_menu() {
 	/*
 	* submenu -> Super Admin, Administrator, Author
 	*/
-	add_submenu_page( $parent_slug, 'Danh sách đơn hàng', 'Đơn hàng', 'publish_posts', $parent_slug, 'func_include_eb_private_code' );
+	if ( current_user_can('publish_posts') )  {
+		add_submenu_page( $parent_slug, 'Danh sách đơn hàng', 'Đơn hàng', 'publish_posts', $parent_slug, 'func_include_eb_private_code' );
+	}
 	
 //	add_submenu_page( $parent_slug, 'Danh sách banner quảng cáo', 'Quảng cáo', 'manage_options', 'eb-ads', 'func_include_eb_private_code' );
 	
