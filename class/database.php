5c4e4c015ca92466a2c3a00e290b16d179fe31d6
itvn9online <itvn9online@gmail.com>
2017-06-20 07:02:52 +0700

    noidung_menu

--- class/database.php
+++ class/database.php
@@ -280,12 +280,12 @@ $__cf_row_default = array(
 	
 	// danh sách tin -> tổng quan
 	'cf_home_column_style' => 'w99',
-	'cf_home_class_style' => 'home_noidung_menu',
+	'cf_home_class_style' => 'noidung_menu',
 	
 	
 	// danh sách tin -> tổng quan
 	'cf_cats_class_style' => 'w99',
-	'cf_cats_column_style' => 'thread_list_noidung_menu',
+	'cf_cats_column_style' => 'noidung_menu',
 	// danh sách tin -> html cho phần node
 	'cf_cats_node_html' => '',
 	// danh sách tin -> số tin trên mỗi dòng
@@ -293,14 +293,14 @@ $__cf_row_default = array(
 	
 	// chi tiết -> tổng quan
 	'cf_post_class_style' => 'w99',
-	'cf_post_column_style' => 'thread_details_noidung_menu',
+	'cf_post_column_style' => 'noidung_menu',
 	// chi tiết -> html cho phần node
 	'cf_post_node_html' => '',
 	
 	
 	// danh sách tin -> tổng quan
 	'cf_blogs_class_style' => 'w99',
-	'cf_blogs_column_style' => 'blogs_noidung_menu',
+	'cf_blogs_column_style' => 'noidung_menu',
 	// danh sách tin -> html cho phần node
 	'cf_blogs_node_html' => '',
 	// danh sách tin -> số tin trên mỗi dòng
@@ -308,7 +308,7 @@ $__cf_row_default = array(
 	
 	// chi tiết -> tổng quan
 	'cf_blogd_class_style' => 'w99',
-	'cf_blog_column_style' => 'blog_details_noidung_menu',
+	'cf_blog_column_style' => 'noidung_menu',
 	// chi tiết -> html cho phần node
 	'cf_blog_node_html' => '',
 	'cf_blog_num_line' => '',
