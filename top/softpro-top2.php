33ab0347f04f0ac7046db02a1ee6f04c4fb6a65e
itvn9online <itvn9online@gmail.com>
2017-07-05 00:45:26 +0700

    softpro-top2

---
+++ top/softpro-top2.php
@@ -0,0 +1,8 @@
+<div class="web-top-logo">
+	<div class="cf <?php echo $__cf_row['cf_top_class_style']; ?>">
+		<div class="lf f25"><a title="<?php echo web_name; ?>" href="./" class="web-logo d-block" style="background-image:url('<?php echo $__cf_row['cf_logo']; ?>');">&nbsp;</a></div>
+		<div class="lf f75">
+			<div class="top-nav upper"><?php echo _eb_echbay_menu( 'top-menu-02' ); ?></div>
+		</div>
+	</div>
+</div>
