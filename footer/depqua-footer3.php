813fc35e382414ed7a56bd6e38594abc7be7d568
itvn9online <itvn9online@gmail.com>
2017-07-04 18:28:39 +0700

    depqua-footer2

---
+++ footer/depqua-footer3.php
@@ -0,0 +1,7 @@
+<div class="bottom-footer text-center l25">
+	<div class="bold"><?php echo $__cf_row['cf_ten_cty']; ?></div>
+	<div>Địa chỉ: <?php echo nl2br( $__cf_row['cf_diachi'] ); ?><br>
+		Hotline: <?php echo $__cf_row['cf_call_hotline']; ?> | Điện thoại: <span class="phone-numbers-inline"><?php echo $__cf_row['cf_call_dienthoai']; ?></span><br>
+		Email: <a href="mailto:<?php echo $__cf_row['cf_email']; ?>" rel="nofollow" target="_blank"><?php echo $__cf_row['cf_email']; ?></a><br>
+		Bản quyền &copy; <?php echo $year_curent; ?> <span><?php echo $web_name; ?></span> - Toàn bộ phiên bản. <?php echo $str_fpr_license_echbay; ?></div>
+</div>
