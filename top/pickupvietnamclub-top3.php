bf86c7f112224291a3a77bfdec56338919b71c85
itvn9online <itvn9online@gmail.com>
2017-07-06 01:30:01 +0700

    pickupvietnamclub-top3

--- top/pickupvietnamclub-top3.php
+++ top/pickupvietnamclub-top3.php
@@ -1,7 +1,7 @@
 <div id="pickupvietnamclub-top3">
 	<div class="top-nav default-bg hide-if-mobile">
-		<div class="cf <?php echo $__cf_row['cf_blog_class_style']; ?>">
-			<div id="nav"><?php echo $arr_tmp_top_menu[1]; ?></div>
+		<div class="cf <?php echo $__cf_row['cf_top_class_style']; ?>">
+			<div class="nav"><?php echo $arr_tmp_top_menu[1]; ?></div>
 		</div>
 	</div>
 </div>
