4a58e6d4487ae9ea9f0fbd04c429c77022f5a7f1
itvn9online <itvn9online@gmail.com>
2017-07-07 04:28:08 +0700

    motocenter-top3

---
+++ top/motocenter-top3.php
@@ -0,0 +1,8 @@
+<div id="motocenter-top3">
+	<div class="web-top-logo cf <?php echo $__cf_row['cf_top_class_style']; ?>">
+		<div class="lf f25 cf fullsize-if-mobile"><a href="./" class="web-logo d-block" style="background-image:url('<?php echo $__cf_row['cf_logo']; ?>');">&nbsp;</a></div>
+		<div class="lf f75 hide-if-mobile">
+			<div class="nav"><?php echo EBE_echbay_top_menu(); ?></div>
+		</div>
+	</div>
+</div>
