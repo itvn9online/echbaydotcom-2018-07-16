864df201419430f20ba99475d695a2a1578a88f4
itvn9online <itvn9online@gmail.com>
2017-07-07 12:23:17 +0700

    footer_default

--- footer_default.php
+++ footer_default.php
@@ -2,8 +2,8 @@
 
 $arr_includes_footer_file[] = EB_THEME_PLUGIN_INDEX . 'footer_widget.php';
 
-$arr_includes_footer_file[] = EB_THEME_PLUGIN_INDEX . 'footer/echbaytwo-footer1.php';
+$arr_includes_footer_file[] = EB_THEME_PLUGIN_INDEX . 'themes/footer/echbaytwo-footer1.php';
 
-$arr_includes_footer_file[] = EB_THEME_PLUGIN_INDEX . 'footer/echbaytwo-footer2.php';
+$arr_includes_footer_file[] = EB_THEME_PLUGIN_INDEX . 'themes/footer/echbaytwo-footer2.php';
 
-$arr_includes_footer_file[] = EB_THEME_PLUGIN_INDEX . 'footer/echbaytwo-footer3.php';
+$arr_includes_footer_file[] = EB_THEME_PLUGIN_INDEX . 'themes/footer/echbaytwo-footer3.php';
--- top_default.php
+++ top_default.php
@@ -1,11 +1,11 @@
 <?php
 
-$arr_includes_top_file[] = EB_THEME_PLUGIN_INDEX . 'top/echbaytwo-top1.php';
+$arr_includes_top_file[] = EB_THEME_PLUGIN_INDEX . 'themes/top/echbaytwo-top1.php';
 
-$arr_includes_top_file[] = EB_THEME_PLUGIN_INDEX . 'top/echbaytwo-top2.php';
+$arr_includes_top_file[] = EB_THEME_PLUGIN_INDEX . 'themes/top/echbaytwo-top2.php';
 
-$arr_includes_top_file[] = EB_THEME_PLUGIN_INDEX . 'top/echbaytwo-top3.php';
+$arr_includes_top_file[] = EB_THEME_PLUGIN_INDEX . 'themes/top/echbaytwo-top3.php';
 
-$arr_includes_top_file[] = EB_THEME_PLUGIN_INDEX . 'top/echbaytwo-top4.php';
+$arr_includes_top_file[] = EB_THEME_PLUGIN_INDEX . 'themes/top/echbaytwo-top4.php';
 
 $arr_includes_top_file[] = EB_THEME_PLUGIN_INDEX . 'top_widget.php';
