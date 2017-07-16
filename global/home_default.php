4b8dfab19937000d660c018e71d0be98f7edcdf6
itvn9online <itvn9online@gmail.com>
2017-07-17 02:32:55 +0700

    home_default

--- global/home_default.php
+++ global/home_default.php
@@ -105,48 +105,51 @@ if ( $__cf_row['cf_num_home_list'] > 0 ) {
 		if ( $home_detauls_categories->count > 0 ) {
 			$home_node_cat = _eb_load_post( $__cf_row['cf_num_home_list'], $args );
 			
-			// danh sách nhóm cấp 2
-			$arr_sub_cat = array(
-				'parent' => $cat_ids,
-			);
-			$sub_cat = get_categories($arr_sub_cat);
-//			print_r( $sub_cat );
-			
-			$str_sub_cat = '';
-			foreach ( $sub_cat as $sub_v ) {
-				$str_sub_cat .= ' <a href="' . _eb_c_link( $sub_v->term_id ) . '">' . $sub_v->name . ' <span class="home-count-subcat">(' . $sub_v->count . ')</span></a>';
-			}
-			
-			
-			
-			// banner quảng cáo theo từng danh mục (cấp 1)
-			$home_ads_by_cat = _eb_load_ads( 9, 5, '', array(
-				'cat' => $cat_ids,
-			), 0, str_replace( 'ti-le-global', '', EBE_get_page_template( 'ads_node' ) ) );
-			
-			
-			
-			// Lấy theo mẫu của widget #home_product
-			$home_with_cat .= EBE_html_template( EBE_get_page_template( 'home_node' ), array(
-				'tmp.cat_id' => $k,
-				'tmp.cat_link' => _eb_c_link( $k ),
-				'tmp.cat_name' => $home_detauls_categories->name,
-				'tmp.cat_count' => $home_detauls_categories->count,
-				'tmp.title' => $home_detauls_categories->name,
-				
+			//
+			if ( $home_node_cat != '' ) {
 				// danh sách nhóm cấp 2
-				'tmp.str_sub_cat' => $str_sub_cat,
+				$arr_sub_cat = array(
+					'parent' => $cat_ids,
+				);
+				$sub_cat = get_categories($arr_sub_cat);
+//				print_r( $sub_cat );
 				
-				// danh sách sản phẩm
-				'tmp.home_node_cat' => $home_node_cat,
+				$str_sub_cat = '';
+				foreach ( $sub_cat as $sub_v ) {
+					$str_sub_cat .= ' <a href="' . _eb_c_link( $sub_v->term_id ) . '">' . $sub_v->name . ' <span class="home-count-subcat">(' . $sub_v->count . ')</span></a>';
+				}
 				
-				// quảng cáo theo nhóm cấp 1
-				'tmp.home_ads_by_cat' => $home_ads_by_cat,
 				
-				// bg chẵn lẻ
-				'tmp.num_post_line' => '',
-				'tmp.max_width' => '',
-			) );
+				
+				// banner quảng cáo theo từng danh mục (cấp 1)
+				$home_ads_by_cat = _eb_load_ads( 9, 5, '', array(
+					'cat' => $cat_ids,
+				), 0, str_replace( 'ti-le-global', '', EBE_get_page_template( 'ads_node' ) ) );
+				
+				
+				
+				// Lấy theo mẫu của widget #home_product
+				$home_with_cat .= EBE_html_template( EBE_get_page_template( 'home_node' ), array(
+					'tmp.cat_id' => $k,
+					'tmp.cat_link' => _eb_c_link( $k ),
+					'tmp.cat_name' => $home_detauls_categories->name,
+					'tmp.cat_count' => $home_detauls_categories->count,
+					'tmp.title' => $home_detauls_categories->name,
+					
+					// danh sách nhóm cấp 2
+					'tmp.str_sub_cat' => $str_sub_cat,
+					
+					// danh sách sản phẩm
+					'tmp.home_node_cat' => $home_node_cat,
+					
+					// quảng cáo theo nhóm cấp 1
+					'tmp.home_ads_by_cat' => $home_ads_by_cat,
+					
+					// bg chẵn lẻ
+					'tmp.num_post_line' => '',
+					'tmp.max_width' => '',
+				) );
+			}
 		}
 	}
 }
