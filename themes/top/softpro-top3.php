cf1bbc51d203e5690c30e91003d1df50da1ff08d
itvn9online <itvn9online@gmail.com>
2017-07-17 09:27:54 +0700

    softpro-top3

---
+++ themes/top/softpro-top3.php
@@ -0,0 +1,10 @@
+<div id="softpro-top3">
+	<div class="thread-details-tohome">
+		<div class="<?php echo $__cf_row['cf_top_class_style']; ?>">
+			<ul class="cf">
+				<li><a href="./"><i class="fa fa-home"></i> Trang chủ</a></li>
+				<?php echo $group_go_to; ?>
+			</ul>
+		</div>
+	</div>
+</div>
