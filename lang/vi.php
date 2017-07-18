a6de6f51b8bcb7cab962be40d3afb96a21d8500f
itvn9online <itvn9online@gmail.com>
2017-07-19 03:18:16 +0700

    quick_cart

--- lang/vi.php
+++ lang/vi.php
@@ -13,6 +13,10 @@ $___eb_lang[eb_key_for_site_lang . 'search'] = 'Tìm kiếm';
 $___eb_lang[eb_key_for_site_lang . 'cart'] = 'Giỏ hàng';
 $___eb_lang[eb_key_for_site_lang . 'mungay'] = 'Mua ngay';
 
+//
+$___eb_lang[eb_key_for_site_lang . 'cart_diachi'] = 'Địa chỉ';
+$___eb_lang[eb_key_for_site_lang . 'cart_dienthoai'] = 'Điện thoại';
+
 // details
 $___eb_lang[eb_key_for_site_lang . 'chitietsp'] = 'Chi tiết Sản phẩm';
 $___eb_lang[eb_key_for_site_lang . 'tuongtu'] = 'Sản phẩm tương tự';
@@ -21,8 +25,8 @@ $___eb_lang[eb_key_for_site_lang . 'tuongtu'] = 'Sản phẩm tương tự';
 $___eb_lang[eb_key_for_site_lang . 'copyright'] = 'Bản quyền';
 $___eb_lang[eb_key_for_site_lang . 'allrights'] = 'Toàn bộ phiên bản';
 $___eb_lang[eb_key_for_site_lang . 'joinus'] = 'Kết nối với chúng tôi';
-$___eb_lang[eb_key_for_site_lang . 'diachi'] = 'Địa chỉ:';
-$___eb_lang[eb_key_for_site_lang . 'dienthoai'] = 'Điện thoại:';
+$___eb_lang[eb_key_for_site_lang . 'diachi'] = $___eb_lang[eb_key_for_site_lang . 'cart_diachi'] . ':';
+$___eb_lang[eb_key_for_site_lang . 'dienthoai'] = $___eb_lang[eb_key_for_site_lang . 'cart_dienthoai'] . ':';
 $___eb_lang[eb_key_for_site_lang . 'poweredby'] = 'Cung cấp bởi';
 
 // echbay two footer sologan
@@ -30,6 +34,19 @@ $___eb_lang[eb_key_for_site_lang . 'ebslogan1'] = '<i class="fa fa-refresh"></i>
 $___eb_lang[eb_key_for_site_lang . 'ebslogan2'] = '<i class="fa fa-truck"></i> Giao hàng miễn phí<br />Toàn Quốc';
 $___eb_lang[eb_key_for_site_lang . 'ebslogan3'] = '<i class="fa fa-dollar"></i> Thanh toán<br />khi giao hàng';
 $___eb_lang[eb_key_for_site_lang . 'ebslogan4'] = '<i class="fa fa-check-square"></i> Bảo hành VIP<br />12 tháng';
+
+// quick cart
+$___eb_lang[eb_key_for_site_lang . 'cart_muangay'] = $___eb_lang[eb_key_for_site_lang . 'mungay'];
+$___eb_lang[eb_key_for_site_lang . 'cart_mausac'] = 'Màu sắc';
+$___eb_lang[eb_key_for_site_lang . 'cart_kichco'] = 'Kích cỡ';
+$___eb_lang[eb_key_for_site_lang . 'cart_soluong'] = 'Số lượng';
+$___eb_lang[eb_key_for_site_lang . 'cart_thanhtien'] = 'Thành tiền';
+$___eb_lang[eb_key_for_site_lang . 'cart_hoten'] = 'Họ và tên';
+$___eb_lang[eb_key_for_site_lang . 'cart_diachi2'] = 'Địa chỉ nhận hàng';
+$___eb_lang[eb_key_for_site_lang . 'cart_ghichu'] = 'Ghi chú';
+$___eb_lang[eb_key_for_site_lang . 'cart_vidu'] = 'Ví dụ: Giao hàng trong giờ hành chính, gọi điện trước khi giao...';
+$___eb_lang[eb_key_for_site_lang . 'cart_gui'] = '<i class="fa fa-shopping-cart"></i> Gửi đơn hàng';
+$___eb_lang[eb_key_for_site_lang . 'cart_them'] = 'Cho vào giỏ hàng';
 
 
 
--- quick_cart.php
+++ quick_cart.php
@@ -5,7 +5,7 @@
 		<div class="cart-quick-close">
 			<div onClick="close_img_quick_video_details();" class="fa fa-close cur"></div>
 		</div>
-		<div class="cart-quick-title bold upper">Mua ngay</div>
+		<div class="cart-quick-title bold upper"><?php echo EBE_get_lang('cart_muangay'); ?></div>
 		<div class="cart-quick-padding">
 			<form name="frm_cart" method="post" action="process/?set_module=booking" target="target_eb_iframe" onsubmit="return _global_js_eb.check_cart();" class="eb-global-frm-cart">
 				<div id="cart_user_agent" class="d-none">
@@ -15,7 +15,7 @@
 				</div>
 				<div class="eb-quickcart-table">
 					<div class="eb-quickcart-node cf show-if-color-exist product-color d-none">
-						<div class="eb-quickcart-left">Màu sắc</div>
+						<div class="eb-quickcart-left"><?php echo EBE_get_lang('cart_mausac'); ?></div>
 						<div class="eb-quickcart-right">
 							<div class="oi_product_color">
 								<ul class="cf">
@@ -24,7 +24,7 @@
 						</div>
 					</div>
 					<div class="eb-quickcart-node cf show-if-size-exist d-none">
-						<div class="eb-quickcart-left">Kích cỡ</div>
+						<div class="eb-quickcart-left"><?php echo EBE_get_lang('cart_kichco'); ?></div>
 						<div class="eb-quickcart-right">
 							<div class="eb-product-size oi_product_size">
 								<ul class="cf">
@@ -33,23 +33,23 @@
 						</div>
 					</div>
 					<div class="eb-quickcart-node eb-quickcart-quan cf">
-						<div class="eb-quickcart-left">Số lượng</div>
+						<div class="eb-quickcart-left"><?php echo EBE_get_lang('cart_soluong'); ?></div>
 						<div id="oi_change_soluong" class="eb-quickcart-right"></div>
 					</div>
 					<div class="eb-quickcart-node eb-quickcart-price cf">
-						<div class="eb-quickcart-left">Thành tiền</div>
+						<div class="eb-quickcart-left"><?php echo EBE_get_lang('cart_thanhtien'); ?></div>
 						<div id="oi_change_tongtien" class="eb-quickcart-right bold redcolor"></div>
 					</div>
 					<div class="eb-quickcart-node cf">
-						<div class="eb-quickcart-left">Họ và tên</div>
+						<div class="eb-quickcart-left"><?php echo EBE_get_lang('cart_hoten'); ?></div>
 						<div class="eb-quickcart-right">
-							<input type="text" name="t_ten" value="" placeholder="Họ và tên" />
+							<input type="text" name="t_ten" value="" placeholder="<?php echo EBE_get_lang('cart_hoten'); ?>" />
 						</div>
 					</div>
 					<div class="eb-quickcart-node cf">
-						<div class="eb-quickcart-left">Điện thoại <span class="redcolor">*</span></div>
+						<div class="eb-quickcart-left"><?php echo EBE_get_lang('cart_dienthoai'); ?> <span class="redcolor">*</span></div>
 						<div class="eb-quickcart-right">
-							<input type="text" name="t_dienthoai" value="" placeholder="Điện thoại" />
+							<input type="text" name="t_dienthoai" value="" placeholder="<?php echo EBE_get_lang('cart_dienthoai'); ?>" />
 						</div>
 					</div>
 					<div class="eb-quickcart-node cf">
@@ -59,20 +59,20 @@
 						</div>
 					</div>
 					<div class="eb-quickcart-node cf">
-						<div class="eb-quickcart-left">Địa chỉ</div>
+						<div class="eb-quickcart-left"><?php echo EBE_get_lang('cart_diachi'); ?></div>
 						<div class="eb-quickcart-right">
-							<textarea name="t_diachi" placeholder="Địa chỉ nhận hàng"></textarea>
+							<textarea name="t_diachi" placeholder="<?php echo EBE_get_lang('cart_diachi2'); ?>"></textarea>
 						</div>
 					</div>
 					<div class="eb-quickcart-node cf">
-						<div class="eb-quickcart-left">Ghi chú</div>
+						<div class="eb-quickcart-left"><?php echo EBE_get_lang('cart_ghichu'); ?></div>
 						<div class="eb-quickcart-right">
-							<textarea name="t_ghichu" placeholder="Ví dụ: Giao hàng trong giờ hành chính, gọi điện trước khi giao..."></textarea>
+							<textarea name="t_ghichu" placeholder="<?php echo EBE_get_lang('cart_vidu'); ?>"></textarea>
 						</div>
 					</div>
 					<div class="text-center">
-						<button type="submit" id="sb_submit_cart" class="default-bg cur"><i class="fa fa-shopping-cart"></i> Gửi đơn hàng</button>
-						<button type="button" class="btn-addto-cart default-2bg div-jquery-add-to-cart click-jquery-add-to-cart">Cho vào giỏ hàng</button>
+						<button type="submit" id="sb_submit_cart" class="default-bg cur"><?php echo EBE_get_lang('cart_gui'); ?></button>
+						<button type="button" class="btn-addto-cart default-2bg div-jquery-add-to-cart click-jquery-add-to-cart"><?php echo EBE_get_lang('cart_them'); ?></button>
 					</div>
 				</div>
 			</form>
