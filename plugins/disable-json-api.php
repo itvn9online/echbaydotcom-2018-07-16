c7a90848cebbeb06ba4bb3ce25c74c0e41c84811
itvn9online <itvn9online@gmail.com>
2017-07-19 20:28:53 +0700

    wp-json

--- plugins/disable-json-api.php
+++ plugins/disable-json-api.php
@@ -81,7 +81,8 @@ function DRA_only_allow_logged_in_rest_access( $access ) {
 // nếu tồn tại tham số rest_route -> đang vào JSON -> hủy luôn
 //if ( isset( $_GET['rest_route'] ) ) {
 // URL mà tồn tại tham số /wp-json/ -> hủy luôn
-if ( strstr( $_SERVER['REQUEST_URI'], '/wp-json/' ) == true ) {
+//if ( strstr( $_SERVER['REQUEST_URI'], '/wp-json/' ) == true ) {
+if ( strstr( $_SERVER['REQUEST_URI'], '/wp-json' ) == true ) {
 
 	// Set trạng thái cho trang 404
 	EBE_set_header(401);
