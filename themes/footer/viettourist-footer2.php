f5d6f760b87fa2908d32c84e8fdedb43aa524e00
itvn9online <itvn9online@gmail.com>
2017-07-20 12:21:19 +0700

    viettourist-footer2

--- themes/footer/viettourist-footer2.php
+++ themes/footer/viettourist-footer2.php
@@ -1,21 +1,22 @@
 <div id="viettourist-footer2">
-	<div class="bottom-support footer-bg">
-		<ul class="footer-social text-center cf">
-			<li class="footer-social-fb"><a href="javascript:;" class="ahref-to-facebook" target="_blank" rel="nofollow"><i class="fa fa-facebook"></i></a></li>
-			<li class="footer-social-tw"><a href="javascript:;" class="each-to-twitter-page" target="_blank" rel="nofollow"><i class="fa fa-twitter"></i></a></li>
-			<li class="footer-social-yt"><a href="javascript:;" class="each-to-youtube-chanel" target="_blank" rel="nofollow"><i class="fa fa-youtube"></i></a></li>
-			<li class="footer-social-gg"><a href="javascript:;" class="ahref-to-gooplus" target="_blank" rel="nofollow"><i class="fa fa-google-plus"></i></a></li>
-		</ul>
-		<br>
-		<div class="text-center">
-			<div class="medium18 upper l30"><?php echo $__cf_row['cf_ten_cty']; ?></div>
-			<ul class="l19">
-				<li><i class="fa fa-map-marker"></i> <?php echo nl2br( $__cf_row['cf_diachi'] ); ?></li>
-				<li><i class="fa fa-phone"></i> Hotline: <span class="phone-numbers-inline"><?php echo $__cf_row['cf_call_hotline']; ?></span> - Điện thoại: <span class="phone-numbers-inline"><?php echo $__cf_row['cf_call_dienthoai']; ?></span></li>
-				<li><i class="fa fa-envelope-o"></i> <a href="mailto:<?php echo $__cf_row['cf_email']; ?>"><?php echo $__cf_row['cf_email']; ?></a></li>
+	<div class="footer-bg">
+		<div class="bottom-support">
+			<ul class="footer-social text-center cf">
+				<li class="footer-social-fb"><a href="javascript:;" class="ahref-to-facebook" target="_blank" rel="nofollow"><i class="fa fa-facebook"></i></a></li>
+				<li class="footer-social-tw"><a href="javascript:;" class="each-to-twitter-page" target="_blank" rel="nofollow"><i class="fa fa-twitter"></i></a></li>
+				<li class="footer-social-yt"><a href="javascript:;" class="each-to-youtube-chanel" target="_blank" rel="nofollow"><i class="fa fa-youtube"></i></a></li>
+				<li class="footer-social-gg"><a href="javascript:;" class="ahref-to-gooplus" target="_blank" rel="nofollow"><i class="fa fa-google-plus"></i></a></li>
 			</ul>
-		</div>
-		<!--
+			<br>
+			<div class="text-center">
+				<div class="medium18 upper l30"><?php echo $__cf_row['cf_ten_cty']; ?></div>
+				<ul class="l19">
+					<li><i class="fa fa-map-marker"></i> <?php echo nl2br( $__cf_row['cf_diachi'] ); ?></li>
+					<li><i class="fa fa-phone"></i> Hotline: <span class="phone-numbers-inline"><?php echo $__cf_row['cf_call_hotline']; ?></span> - Điện thoại: <span class="phone-numbers-inline"><?php echo $__cf_row['cf_call_dienthoai']; ?></span></li>
+					<li><i class="fa fa-envelope-o"></i> <a href="mailto:<?php echo $__cf_row['cf_email']; ?>"><?php echo $__cf_row['cf_email']; ?></a></li>
+				</ul>
+			</div>
+			<!--
 		<div class="cf w99 d-none">
 			<div class="lf f62 fullsize-if-mobile">
 				
@@ -36,5 +37,6 @@
 			</div>
 		</div>
 		--> 
+		</div>
 	</div>
 </div>
