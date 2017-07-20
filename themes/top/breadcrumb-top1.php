1cd9361518f7af43b15003d05657766ef5c87b72
itvn9online <itvn9online@gmail.com>
2017-07-20 12:44:37 +0700

    breadcrumb-top1

---
+++ themes/top/breadcrumb-top1.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Description: Sử dụng thiết kế tràn khung, tràn màn hình
+*/
+?>
+
+<div id="breadcrumb-top1">
+	<div class="thread-details-tohome">
+		<div class="<?php echo $__cf_row['cf_top_class_style']; ?>">
+			<ul class="cf">
+				<li><a href="./"><i class="fa fa-home"></i> <?php echo EBE_get_lang('home'); ?></a></li>
+				<?php echo $group_go_to; ?>
+			</ul>
+		</div>
+	</div>
+</div>
---
+++ themes/top/breadcrumb-top2.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Description: Thiết kế bo gọn theo kích thước của TOP trong mục Cài đặt giao diện
+*/
+?>
+
+<div id="breadcrumb-top2">
+	<div class="<?php echo $__cf_row['cf_top_class_style']; ?>">
+		<div class="thread-details-tohome">
+			<ul class="cf">
+				<li><a href="./"><i class="fa fa-home"></i> <?php echo EBE_get_lang('home'); ?></a></li>
+				<?php echo $group_go_to; ?>
+			</ul>
+		</div>
+	</div>
+</div>
