a98cc28742a44f1eb4ec022b4ff0e98127acedce
itvn9online <itvn9online@gmail.com>
2017-07-21 13:01:41 +0700

    pickupvietnamclub-top3

--- themes/top/pickupvietnamclub-top3.php
+++ themes/top/pickupvietnamclub-top3.php
@@ -1,7 +1,7 @@
 <div id="pickupvietnamclub-top3">
 	<div class="top-nav default-bg hide-if-mobile">
 		<div class="cf <?php echo $__cf_row['cf_top_class_style']; ?>">
-			<div class="nav"><?php echo $arr_tmp_top_menu[1]; ?></div>
+			<div class="nav"><?php echo EBE_echbay_top_menu(); ?></div>
 		</div>
 	</div>
 </div>
