6bb76a933e337db7a86fba91042376e65974677c
itvn9online <itvn9online@gmail.com>
2017-07-25 18:39:48 +0700

    buttom-for-quick-add-menu

--- echbay/global_html.php
+++ echbay/global_html.php
@@ -1,32 +1,30 @@
 <div class="d-none">
 	<div id="content-for-quick-add-menu">
-		<div>
-			<ul>
-				<li class="cf">
-					<div class="lf f75"><?php echo EBE_get_lang('home'); ?></div>
-					<div class="lf f25 text-center">
-						<button data-link="/" data-text="<?php echo EBE_get_lang('home'); ?>" type="button" class="cur click-to-add-custom-link"><i class="fa fa-plus"></i></button>
-					</div>
-				</li>
-				<li class="cf">
-					<div class="lf f75"><?php echo EBE_get_lang('lienhe'); ?></div>
-					<div class="lf f25 text-center">
-						<button data-link="/contact" data-text="<?php echo EBE_get_lang('lienhe'); ?>" type="button" class="cur click-to-add-custom-link"><i class="fa fa-plus"></i></button>
-					</div>
-				</li>
-				<li class="cf">
-					<div class="lf f75"><?php echo EBE_get_lang('cart'); ?></div>
-					<div class="lf f25 text-center">
-						<button data-link="/cart" data-text="<?php echo EBE_get_lang('cart'); ?>" type="button" class="cur click-to-add-custom-link"><i class="fa fa-plus"></i></button>
-					</div>
-				</li>
-				<li class="cf">
-					<div class="lf f75"><?php echo EBE_get_lang('taikhoan'); ?></div>
-					<div class="lf f25 text-center">
-						<button data-link="/profile" data-text="<?php echo EBE_get_lang('taikhoan'); ?>" type="button" class="cur click-to-add-custom-link"><i class="fa fa-plus"></i></button>
-					</div>
-				</li>
-			</ul>
-		</div>
+		<ul class="buttom-for-quick-add-menu">
+			<li class="cf">
+				<div class="lf f75"><?php echo EBE_get_lang('home'); ?></div>
+				<div class="lf f25 text-center">
+					<button data-link="/" data-text="<?php echo EBE_get_lang('home'); ?>" type="button" class="cur click-to-add-custom-link"><i class="fa fa-plus"></i></button>
+				</div>
+			</li>
+			<li class="cf">
+				<div class="lf f75"><?php echo EBE_get_lang('lienhe'); ?></div>
+				<div class="lf f25 text-center">
+					<button data-link="/contact" data-text="<?php echo EBE_get_lang('lienhe'); ?>" type="button" class="cur click-to-add-custom-link"><i class="fa fa-plus"></i></button>
+				</div>
+			</li>
+			<li class="cf">
+				<div class="lf f75"><?php echo EBE_get_lang('cart'); ?></div>
+				<div class="lf f25 text-center">
+					<button data-link="/cart" data-text="<?php echo EBE_get_lang('cart'); ?>" type="button" class="cur click-to-add-custom-link"><i class="fa fa-plus"></i></button>
+				</div>
+			</li>
+			<li class="cf">
+				<div class="lf f75"><?php echo EBE_get_lang('taikhoan'); ?></div>
+				<div class="lf f25 text-center">
+					<button data-link="/profile" data-text="<?php echo EBE_get_lang('taikhoan'); ?>" type="button" class="cur click-to-add-custom-link"><i class="fa fa-plus"></i></button>
+				</div>
+			</li>
+		</ul>
 	</div>
 </div>
