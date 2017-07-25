a6efb13e3cf0f6de597681ede955ac82dca06796
itvn9online <itvn9online@gmail.com>
2017-07-25 18:44:41 +0700

    buttom-for-quick-add-menu

--- echbay/global_html.php
+++ echbay/global_html.php
@@ -1,27 +1,31 @@
 <div class="d-none">
 	<div id="content-for-quick-add-menu">
 		<ul class="buttom-for-quick-add-menu">
+			<li>
+				<h4>Thêm menu nhanh</h4>
+				<div>Bấm dấu + để thêm menu!</div>
+			</li>
 			<li class="cf">
-				<div class="lf f75"><?php echo EBE_get_lang('home'); ?></div>
-				<div class="lf f25 text-center">
+				<div class="lf f80"><?php echo EBE_get_lang('home'); ?></div>
+				<div class="lf f20 text-center">
 					<button data-link="/" data-text="<?php echo EBE_get_lang('home'); ?>" type="button" class="cur click-to-add-custom-link"><i class="fa fa-plus"></i></button>
 				</div>
 			</li>
 			<li class="cf">
-				<div class="lf f75"><?php echo EBE_get_lang('lienhe'); ?></div>
-				<div class="lf f25 text-center">
+				<div class="lf f80"><?php echo EBE_get_lang('lienhe'); ?></div>
+				<div class="lf f20 text-center">
 					<button data-link="/contact" data-text="<?php echo EBE_get_lang('lienhe'); ?>" type="button" class="cur click-to-add-custom-link"><i class="fa fa-plus"></i></button>
 				</div>
 			</li>
 			<li class="cf">
-				<div class="lf f75"><?php echo EBE_get_lang('cart'); ?></div>
-				<div class="lf f25 text-center">
+				<div class="lf f80"><?php echo EBE_get_lang('cart'); ?></div>
+				<div class="lf f20 text-center">
 					<button data-link="/cart" data-text="<?php echo EBE_get_lang('cart'); ?>" type="button" class="cur click-to-add-custom-link"><i class="fa fa-plus"></i></button>
 				</div>
 			</li>
 			<li class="cf">
-				<div class="lf f75"><?php echo EBE_get_lang('taikhoan'); ?></div>
-				<div class="lf f25 text-center">
+				<div class="lf f80"><?php echo EBE_get_lang('taikhoan'); ?></div>
+				<div class="lf f20 text-center">
 					<button data-link="/profile" data-text="<?php echo EBE_get_lang('taikhoan'); ?>" type="button" class="cur click-to-add-custom-link"><i class="fa fa-plus"></i></button>
 				</div>
 			</li>
