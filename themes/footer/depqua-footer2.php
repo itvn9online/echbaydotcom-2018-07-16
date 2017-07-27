de8873fc66beb514017915b9ceb425de5331c4d6
itvn9online <itvn9online@gmail.com>
2017-07-27 22:55:31 +0700

    gift house img

--- themes/footer/depqua-footer2.php
+++ themes/footer/depqua-footer2.php
@@ -1,68 +1,70 @@
-<div class="<?php echo $__cf_row['cf_footer_class_style']; ?>">
-	<div class="cf">
-		<div class="lf f25 fullsize-if-mobile l19">
-			<div class="right-menu-space">
-				<?php
-				echo EBE_echbay_footer_menu(
-					array(
-						'menu_class' => 'bottom-node',
-					),
-					1,
-					'<div class="footer-title">'
-				);
-				?>
+<div id="depqua-footer2">
+	<div class="<?php echo $__cf_row['cf_footer_class_style']; ?>">
+		<div class="cf">
+			<div class="lf f25 fullsize-if-mobile l19">
+				<div class="right-menu-space">
+					<?php
+					echo EBE_echbay_footer_menu(
+						array(
+							'menu_class' => 'bottom-node',
+						),
+						1,
+						'<div class="footer-title">'
+					);
+					?>
+				</div>
+				<br>
 			</div>
-			<br>
-		</div>
-		<div class="lf f25 fullsize-if-mobile l19">
-			<div class="right-menu-space">
-				<?php
-				echo EBE_echbay_footer_menu(
-					array(
-						'menu_class' => 'bottom-node',
-					),
-					1,
-					'<div class="footer-title">'
-				);
-				?>
+			<div class="lf f25 fullsize-if-mobile l19">
+				<div class="right-menu-space">
+					<?php
+					echo EBE_echbay_footer_menu(
+						array(
+							'menu_class' => 'bottom-node',
+						),
+						1,
+						'<div class="footer-title">'
+					);
+					?>
+				</div>
+				<br>
 			</div>
-			<br>
-		</div>
-		<div class="lf f25 fullsize-if-mobile l19">
-			<div class="right-menu-space">
-				<?php
-				echo EBE_echbay_footer_menu(
-					array(
-						'menu_class' => 'bottom-node',
-					),
-					1,
-					'<div class="footer-title">'
-				);
-				?>
+			<div class="lf f25 fullsize-if-mobile l19">
+				<div class="right-menu-space">
+					<?php
+					echo EBE_echbay_footer_menu(
+						array(
+							'menu_class' => 'bottom-node',
+						),
+						1,
+						'<div class="footer-title">'
+					);
+					?>
+				</div>
+				<br>
 			</div>
-			<br>
-		</div>
-		<div class="lf f25 l25 fullsize-if-mobile">
-			<div>
-				<?php
-				echo EBE_echbay_footer_menu(
-					array(
-						'menu_class' => 'bold bottom-contact',
-					),
-					1,
-					'<div class="footer-title upper">'
-				);
-				?>
+			<div class="lf f25 l25 fullsize-if-mobile">
+				<div>
+					<?php
+					echo EBE_echbay_footer_menu(
+						array(
+							'menu_class' => 'bold bottom-contact',
+						),
+						1,
+						'<div class="footer-title upper">'
+					);
+					?>
+				</div>
+				<hr class="orgborder one-line" />
+				<div><i class="fa fa-star greencolor"></i> Kết nối với chúng tôi</div>
+				<ul class="footer-social text-center cf">
+					<li class="footer-social-fb"><a href="javascript:;" class="ahref-to-facebook" target="_blank" rel="nofollow"><i class="fa fa-facebook"></i></a></li>
+					<li class="footer-social-tw"><a href="javascript:;" class="each-to-twitter-page" target="_blank" rel="nofollow"><i class="fa fa-twitter"></i></a></li>
+					<li class="footer-social-yt"><a href="javascript:;" class="each-to-youtube-chanel" target="_blank" rel="nofollow"><i class="fa fa-youtube"></i></a></li>
+					<li class="footer-social-gg"><a href="javascript:;" class="ahref-to-gooplus" target="_blank" rel="nofollow"><i class="fa fa-google-plus"></i></a></li>
+				</ul>
+				<br>
 			</div>
-			<hr class="orgborder one-line" />
-			<div><i class="fa fa-star greencolor"></i> Kết nối với chúng tôi</div>
-			<ul class="footer-social text-center cf">
-				<li class="footer-social-fb"><a href="javascript:;" class="ahref-to-facebook" target="_blank" rel="nofollow"><i class="fa fa-facebook"></i></a></li>
-				<li class="footer-social-tw"><a href="javascript:;" class="each-to-twitter-page" target="_blank" rel="nofollow"><i class="fa fa-twitter"></i></a></li>
-				<li class="footer-social-yt"><a href="javascript:;" class="each-to-youtube-chanel" target="_blank" rel="nofollow"><i class="fa fa-youtube"></i></a></li>
-				<li class="footer-social-gg"><a href="javascript:;" class="ahref-to-gooplus" target="_blank" rel="nofollow"><i class="fa fa-google-plus"></i></a></li>
-			</ul>
-			<br>
 		</div>
 	</div>
 </div>
