3023a22f8ac2668d93826a6dace54981c848853a
itvn9online <itvn9online@gmail.com>
2017-07-27 23:15:14 +0700

    remove new theme, set to echbaytwo

--- themes/top/giftHouse-top2.php
+++
@@ -1,6 +0,0 @@
-<div id="giftHouse-top2">
-	<div class="cf <?php echo $__cf_row['cf_top_class_style']; ?>">
-		<div><a href="./" title="Trang chủ" class="web-logo" style="background-image:url('<?php echo $__cf_row['cf_logo']; ?>');">&nbsp;</a></div>
-		<div class="nav-ngang hide-if-mobile"><?php echo EBE_echbay_top_menu(); ?></div>
-	</div>
-</div>
