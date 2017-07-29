d57c90dde109575aa9711cae588d30cf8b48f4fb
itvn9online <itvn9online@gmail.com>
2017-07-29 15:15:06 +0700

    bigbannerFixed-top1

--- themes/top/bigbannerFixed-top1.php
+++ themes/top/bigbannerFixed-top1.php
@@ -3,6 +3,9 @@
 Description: Banner chính, banner lớn của website, có thể sử dụng thay thế cho mục widget nếu muốn. Chiều rộng được giới hạn theo kích thước của TOP trong mục Cài đặt giao diện.
 */
 ?>
-<div id="bigbanner-top1">
-	<div class="oi_big_banner"><?php echo $str_big_banner; ?></div>
+
+<div id="bigbannerFixed-top1">
+	<div class="<?php echo $__cf_row['cf_top_class_style']; ?>">
+		<div class="oi_big_banner"><?php echo $str_big_banner; ?></div>
+	</div>
 </div>
