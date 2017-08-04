1905cdf58fa3369a1046c26d96e5bc0a880e28dc
itvn9online <itvn9online@gmail.com>
2017-08-04 11:08:00 +0700

    remove big banner in page

--- global/details.php
+++ global/details.php
@@ -258,9 +258,13 @@ if ( isset( $cats[0] ) ) {
 
 // Chỉ lấy banner riêng khi chế độ global không được kích hoạt
 if ( $__cf_row['cf_global_big_banner'] == 0 ) {
-	$str_big_banner = EBE_get_big_banner( 5, array(
-		'category__in' => '',
-	) );
+	// Mặc định chỉ lấy cho phần post
+	if ( $cid > 0 ) {
+		$str_big_banner = EBE_get_big_banner( 5, array(
+			'category__in' => '',
+		) );
+	}
+	// còn đây là page -> chưa làm
 }
 
 
