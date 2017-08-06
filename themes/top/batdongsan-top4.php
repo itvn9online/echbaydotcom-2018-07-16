d329868f3bcdf27e44c574b1f927e6f9d4d69a9c
itvn9online <itvn9online@gmail.com>
2017-08-07 04:28:17 +0700

    img for batdongsan theme

--- themes/top/batdongsan-top4.php
+++
@@ -1,7 +0,0 @@
-<div id="batdongsan-top4">
-	<div class="<?php echo $__cf_row['cf_top_class_style']; ?>">
-		<div class="batdongsan-top4">
-			<!-- Write HTML code to here -->
-		</div>
-	</div>
-</div>
