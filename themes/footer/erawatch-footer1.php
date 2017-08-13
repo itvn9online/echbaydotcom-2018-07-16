12830448743bedb159853350cc7a1ad4f0b569a6
itvn9online <itvn9online@gmail.com>
2017-08-13 09:12:04 +0700

    css for thread details

--- themes/footer/erawatch-footer1.php
+++ themes/footer/erawatch-footer1.php
@@ -1,63 +1,47 @@
 <div id="erawatch-footer1">
 	<div class="<?php echo $__cf_row['cf_footer_class_style']; ?>">
-		<div class="erawatch-footer1">
+		<div class="erawatch-footer1 footer-news">
 			<div class="lf f25 fullsize-if-mobile">
 				<?php
 				echo EBE_echbay_footer_menu(
-					array(
-						'menu_class' => 'footer-about-menu cf',
-					),
+					array(),
 					1,
-					'<div class="medium18 upper">',
-					'</div><br>'
+					'<div class="footer-news-title">'
 				);
 				?>
-				<br>
 			</div>
 			<div class="lf f25 fullsize-if-mobile">
 				<div class="left-menu-space">
 					<?php
 					echo EBE_echbay_footer_menu(
-						array(
-							'menu_class' => 'footer-about-menu cf',
-						),
+						array(),
 						1,
-						'<div class="medium18 upper">',
-						'</div><br>'
+						'<div class="footer-news-title">'
 					);
 					?>
 				</div>
-				<br>
 			</div>
 			<div class="lf f25 fullsize-if-mobile">
 				<div class="left-menu-space">
 					<?php
 					echo EBE_echbay_footer_menu(
-						array(
-							'menu_class' => 'footer-about-menu cf',
-						),
+						array(),
 						1,
-						'<div class="medium18 upper">',
-						'</div><br>'
+						'<div class="footer-news-title">'
 					);
 					?>
 				</div>
-				<br>
 			</div>
 			<div class="lf f25 fullsize-if-mobile">
 				<div class="left-menu-space">
 					<?php
 						echo EBE_echbay_footer_menu(
-						array(
-							'menu_class' => 'footer-about-menu cf',
-						),
+						array(),
 						1,
-						'<div class="medium18 upper">',
-						'</div><br>'
+						'<div class="footer-news-title">'
 					);
 					?>
 				</div>
-				<br>
 			</div>
 		</div>
 	</div>
