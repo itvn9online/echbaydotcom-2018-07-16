777bc1e834b5047323a62c4cd3da85276ceba171
itvn9online <itvn9online@gmail.com>
2017-08-14 10:43:04 +0700

    fixed update config

--- echbay/func/config.php
+++ echbay/func/config.php
@@ -272,8 +272,8 @@ foreach( $_POST as $k => $v ) {
 	
 	// hải có chữ cf_ ở đầu tiền
 //	if ( substr( $k, 0, 3 ) == 'cf_' ) {
-	if ( substr( $k, 0, 3 ) == 'cf_' && isset( $__cf_row_default[ $k ] ) ) {
-		if ( substr( $k, 0, 3 ) == 'cf_' && isset( $__cf_row_default[ $k ] ) ) {
+	if ( substr( $k, 0, 3 ) == 'cf_' ) {
+		if ( isset( $__cf_row_default[ $k ] ) ) {
 //			echo 'insert<br>';
 //			echo $v . '<br>';
 			
