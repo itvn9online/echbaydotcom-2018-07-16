300560434e080af1ff78c5432e0c5b53b8a3f51f
itvn9online <itvn9online@gmail.com>
2017-08-31 11:33:47 +0700

    vinhaninteriorV2-home3

---
+++ themes/home/vinhaninteriorV2-home3.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Description: Sử dụng nhân của bản echbaytwo-home1 sau đó CSS lại cho phù hợp với giao diện mong muốn của vinhaninterior.
+*/
+?>
+
+<div id="vinhaninteriorV2-home3">
+	<?php
+	include EB_THEME_PLUGIN_INDEX . 'themes/top/echbaytwo-home1.php';
+	$arr_for_add_theme_css[ EB_THEME_PLUGIN_INDEX . 'css/echbaytwo-home1.css' ] = 1;
+	?>
+</div>
