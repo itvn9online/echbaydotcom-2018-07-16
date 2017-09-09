4e7316084627a70422aaabc0633abf10d3c061b2
itvn9online <itvn9online@gmail.com>
2017-09-09 12:02:29 +0700

    product-color li.text-color-center

--- themes/footer/noithattrevietnam-footer2.php
+++ themes/footer/noithattrevietnam-footer2.php
@@ -4,16 +4,19 @@
 			<div class="lf f25 hide-if-mobile">
 				<div class="right-menu-space"><?php echo EBE_get_html_logo(); ?></div>
 			</div>
-			<div class="lf f75 fullsize-if-mobile">
+			<div class="lf f50 fullsize-if-mobile">
 				<div class="bold upper medium l35"><?php echo $__cf_row['cf_ten_cty']; ?></div>
 				<div class="cf">
-					<div class="lf f38 fullsize-if-mobile">
+					<div class="lf f50 fullsize-if-mobile">
 						<div class="bold upper l35">Hà Nội</div>
 						<div><?php echo EBE_get_html_address(); ?></div>
 					</div>
-					<div class="lf f62 fullsize-if-mobile"><?php echo EBE_echbay_footer_menu(); ?></div>
+					<div class="lf f50 fullsize-if-mobile">
+						<div class="left-menu-space footer-menu"><?php echo EBE_echbay_footer_menu(); ?></div>
+					</div>
 				</div>
 			</div>
+			<div class="lf f25 fullsize-if-mobile"><?php echo WGR_get_fb_like_box(); ?></div>
 		</div>
 	</div>
 </div>
