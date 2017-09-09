eb6d4e19a0612a2c64e831be9f8503e5b96d6402
itvn9online <itvn9online@gmail.com>
2017-09-09 23:55:06 +0700

    vietnamontrails

---
+++ themes/all/vietnamontrails.php
@@ -0,0 +1,23 @@
+<?php 
+/*
+* Save from Mon, 07 Aug 2017 20:52:56 +0700
+* By itvn9online@yahoo.com #1
+* From IP: 127.0.0.1
+* On domain: localhost:8888
+* User agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36
+*/
+$eb_all_themes_support["vietnamontrails"]=array();
+$eb_all_themes_support["vietnamontrails"]["screenshot"] = "https://img1.webgiare.org/vietnamontrails.jpg";
+//
+$eb_all_themes_support["vietnamontrails"]["cf_top1_include_file"] = "top_widget.php";
+$eb_all_themes_support["vietnamontrails"]["cf_top2_include_file"] = "vietnamontrails-top3";
+$eb_all_themes_support["vietnamontrails"]["cf_top3_include_file"] = "echbaytwo-top4.php";
+//
+$eb_all_themes_support["vietnamontrails"]["cf_footer1_include_file"] = "footer_widget.php";
+$eb_all_themes_support["vietnamontrails"]["cf_footer2_include_file"] = "vietnamontrails-footer1.php";
+$eb_all_themes_support["vietnamontrails"]["cf_footer3_include_file"] = "vietnamontrails-footer2.php";
+$eb_all_themes_support["vietnamontrails"]["cf_footer4_include_file"] = "echbaytwo-footer3.php";
+//
+$eb_all_themes_support["vietnamontrails"]["cf_threaddetails_include_file"] = "vietnamontrails-threaddetails.html";
+//
+$eb_all_themes_support["vietnamontrails"]["cf_threadnode_include_file"] = "vietnamontrails-threadnode.html";
