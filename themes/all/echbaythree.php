7cb13a02752dc28425def3ffac2e6059b0863d85
itvn9online <itvn9online@gmail.com>
2017-09-16 07:29:28 +0700

    nut cho big banner
    cho cái nút được ưu tiên hơn so với phần link, để có thể ấn được

---
+++ themes/all/echbaythree.php
@@ -0,0 +1,33 @@
+<?php 
+/*
+* Save from Sat, 16 Sep 2017 00:45:23 +0700
+* By itvn9online@yahoo.com #1
+* From IP: 127.0.0.1
+* On domain: localhost:8888
+* User agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36
+*/
+$eb_all_themes_support["echbaythree"] = array();
+$eb_all_themes_support["echbaythree"]["name"] = "echbaythree";
+$eb_all_themes_support["echbaythree"]["screenshot"] = "https://img1.webgiare.org/echbaythree.jpg";
+//
+$eb_all_themes_support["echbaythree"]["cf_top1_include_file"] = "echbaythree-top1.php";
+$eb_all_themes_support["echbaythree"]["cf_top2_include_file"] = "echbaythree-top2.php";
+$eb_all_themes_support["echbaythree"]["cf_top3_include_file"] = "echbaythree-top3.php";
+$eb_all_themes_support["echbaythree"]["cf_top4_include_file"] = "echbaythree-top4.php";
+$eb_all_themes_support["echbaythree"]["cf_top5_include_file"] = "top_widget.php";
+//
+$eb_all_themes_support["echbaythree"]["cf_footer1_include_file"] = "footer_widget.php";
+$eb_all_themes_support["echbaythree"]["cf_footer2_include_file"] = "echbaythree-footer2.php";
+$eb_all_themes_support["echbaythree"]["cf_footer3_include_file"] = "echbaythree-footer3.php";
+$eb_all_themes_support["echbaythree"]["cf_footer4_include_file"] = "echbaytwo-footer3.php";
+//
+$eb_all_themes_support["echbaythree"]["cf_home1_include_file"] = "echbaythree-home1.php";
+$eb_all_themes_support["echbaythree"]["cf_home2_include_file"] = "echbaythree-home2.php";
+$eb_all_themes_support["echbaythree"]["cf_home3_include_file"] = "echbaythree-home3.php";
+$eb_all_themes_support["echbaythree"]["cf_home4_include_file"] = "echbaythree-home4.php";
+$eb_all_themes_support["echbaythree"]["cf_home5_include_file"] = "echbaythree-home5.php";
+$eb_all_themes_support["echbaythree"]["cf_home6_include_file"] = "home_widget.php";
+//
+$eb_all_themes_support["echbaythree"]["cf_threaddetails_include_file"] = "echbaythree-threaddetails.html";
+//
+$eb_all_themes_support["echbaythree"]["cf_threadnode_include_file"] = "echbaythree-threadnode.html";
