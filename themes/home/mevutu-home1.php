8042de2c80fb2d14ba11313f95b9319c17057ae3
itvn9online <itvn9online@gmail.com>
2017-09-21 07:01:12 +0700

    mevutu

--- themes/home/mevutu-home1.php
+++ themes/home/mevutu-home1.php
@@ -1,3 +1,5 @@
 <div id="mevutu-home1">
-	<div style="text-align: justify;"><?php echo _eb_load_ads( 6, 2 ); ?></div>
+	<div class="home-top1-banner" style="text-align: justify;"><?php echo _eb_load_ads( 6, 3, '1', array(), 0, '', array(
+		'add_class' => 'echbay-blog echbay-blog33'
+	) ); ?></div>
 </div>
--- themes/home/mevutu-home3.php
+++ themes/home/mevutu-home3.php
@@ -1,3 +1,5 @@
 <div id="mevutu-home3">
-	<div style="text-align: justify;"><?php echo _eb_load_ads( 8, 1, 'auto' ); ?></div>
+	<div class="home-top3-banner" style="text-align: justify;"><?php echo _eb_load_ads( 8, 1, 'auto', array(), 0, '', array(
+		'add_class' => 'echbay-blog100'
+	) ); ?></div>
 </div>
