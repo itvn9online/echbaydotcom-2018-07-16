9f9a9ebbdfd44298a19c98b22cbb3604996024fa
itvn9online <itvn9online@gmail.com>
2017-09-26 03:24:51 +0700

    vicwatch

---
+++ themes/all/vicwatch.php
@@ -0,0 +1,26 @@
+<?php 
+/*
+* Save from Mon, 07 Aug 2017 20:52:56 +0700
+* By itvn9online@yahoo.com #1
+* From IP: 127.0.0.1
+* On domain: localhost:8888
+* User agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36
+*/
+$eb_all_themes_support["vicwatch"]=array();
+$eb_all_themes_support["vicwatch"]["screenshot"] = "https://img1.webgiare.org/vicwatch.jpg";
+//
+$eb_all_themes_support["vicwatch"]["cf_top1_include_file"] = "vicwatch-top1.php";
+$eb_all_themes_support["vicwatch"]["cf_top2_include_file"] = "vicwatch-top2.php";
+$eb_all_themes_support["vicwatch"]["cf_top3_include_file"] = "vicwatch-top3.php";
+$eb_all_themes_support["vicwatch"]["cf_top4_include_file"] = "top_widget.php";
+$eb_all_themes_support["vicwatch"]["cf_top5_include_file"] = "bigbanner-top1.php";
+$eb_all_themes_support["vicwatch"]["cf_top6_include_file"] = "echbaytwo-top4.php";
+//
+$eb_all_themes_support["vicwatch"]["cf_footer1_include_file"] = "footer_widget.php";
+$eb_all_themes_support["vicwatch"]["cf_footer2_include_file"] = "vicwatch-footer1.php";
+$eb_all_themes_support["vicwatch"]["cf_footer3_include_file"] = "vicwatch-footer2.php";
+$eb_all_themes_support["vicwatch"]["cf_footer4_include_file"] = "echbaytwo-footer3.php";
+//
+//$eb_all_themes_support["vicwatch"]["cf_threaddetails_include_file"] = "vicwatch-threaddetails.html";
+//
+$eb_all_themes_support["vicwatch"]["cf_threadnode_include_file"] = "vicwatch-threadnode.html";
