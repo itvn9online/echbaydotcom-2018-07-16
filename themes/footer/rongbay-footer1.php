47ff5be456c9237f94d480a6131a0044448a46f4
itvn9online <itvn9online@gmail.com>
2017-09-27 02:41:39 +0700

    set private theme

--- themes/footer/rongbay-footer1.php
+++
@@ -1,5 +0,0 @@
-<div id="rongbay-footer1">
-	<div class="<?php echo $__cf_row['cf_footer_class_style']; ?>">
-		<div class="rongbay-footer1"><?php echo EBE_echbay_footer_menu(); ?></div>
-	</div>
-</div>
--- themes/footer/rongbay-footer3.php
+++
@@ -1,5 +0,0 @@
-<div id="rongbay-footer3">
-	<div class="<?php echo $__cf_row['cf_footer_class_style']; ?>">
-		<div class="rongbay-footer3"><?php echo EBE_echbay_category_menu('post_tag'); ?></div>
-	</div>
-</div>
