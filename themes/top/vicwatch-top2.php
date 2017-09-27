ca560952f965855f12322854c93db6b6bfcb971c
itvn9online <itvn9online@gmail.com>
2017-09-27 17:21:38 +0700

    vicwatch

--- themes/top/vicwatch-top2.php
+++
@@ -1,3 +0,0 @@
-<div id="vicwatch-top2">
-	<div class="<?php echo $__cf_row['cf_top_class_style']; ?>"><?php echo EBE_get_html_logo(); ?></div>
-</div>
--- themes/top/vicwatch-top3.php
+++
@@ -1,3 +0,0 @@
-<div id="vicwatch-top3">
-	<div class="nav-menu <?php echo $__cf_row['cf_top_class_style']; ?> hide-if-mobile"><?php echo EBE_echbay_top_menu(); ?></div>
-</div>
