f378aed89565d2ba0cc4ab0d45a9d244e8cec55a
itvn9online <itvn9online@gmail.com>
2017-09-30 04:59:26 +0700

    sandi vietnam theme

---
+++ themes/all/sandivietnam.php
@@ -0,0 +1,32 @@
+<?php 
+/*
+* Save from Sat, 30 Sep 2017 04:42:35 +0700
+* By itvn9online@yahoo.com #1
+* From IP: 127.0.0.1
+* On domain: localhost:8888
+* User agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36
+*/
+$eb_all_themes_support["sandivietnam"] = array();
+$eb_all_themes_support["sandivietnam"]["name"] = "sandivietnam";
+$eb_all_themes_support["sandivietnam"]["screenshot"] = "https://img1.webgiare.org/sandivietnam.jpg";
+$eb_all_themes_support["sandivietnam"]["author"] = "";
+$eb_all_themes_support["sandivietnam"]["price"] = 500000;
+//
+$eb_all_themes_support["sandivietnam"]["cf_top1_include_file"] = "sandivietnam-top1.php";
+$eb_all_themes_support["sandivietnam"]["top"] = "top_widget.php";
+$eb_all_themes_support["sandivietnam"]["top"] = "breadcrumb-top1.php";
+//
+$eb_all_themes_support["sandivietnam"]["cf_footer1_include_file"] = "footer_widget.php";
+$eb_all_themes_support["sandivietnam"]["cf_footer1_include_file"] = "sandivietnam-footer1.php";
+$eb_all_themes_support["sandivietnam"]["footer"] = "echbaytwo-footer3.php";
+//
+$eb_all_themes_support["sandivietnam"]["cf_home1_include_file"] = "sandivietnam-home1.php";
+$eb_all_themes_support["sandivietnam"]["cf_home2_include_file"] = "sandivietnam-home2.php";
+$eb_all_themes_support["sandivietnam"]["cf_home3_include_file"] = "sandivietnam-home3.php";
+$eb_all_themes_support["sandivietnam"]["cf_home4_include_file"] = "sandivietnam-home4.php";
+$eb_all_themes_support["sandivietnam"]["cf_home5_include_file"] = "sandivietnam-home5.php";
+$eb_all_themes_support["sandivietnam"]["home"] = "home_widget.php";
+//
+$eb_all_themes_support["sandivietnam"]["cf_threaddetails_include_file"] = "sandivietnam-threaddetails.html";
+//
+$eb_all_themes_support["sandivietnam"]["cf_threadnode_include_file"] = "sandivietnam-threadnode.html";
