938006f8b646cde7900304c5a9638c4aa34c87b6
itvn9online <itvn9online@gmail.com>
2017-09-30 01:50:17 +0700

    softpro theme

---
+++ themes/all/softpro.php
@@ -0,0 +1,25 @@
+<?php 
+/*
+* create in Sun, 06 Aug 2017 20:27:13 +0700
+* by itvn9online@yahoo.com #1
+* from IP: 127.0.0.1
+* on domain: localhost:8888
+* with browser: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36
+*/
+$eb_all_themes_support["softpro"]=array();
+$eb_all_themes_support["softpro"]["screenshot"] = "https://img1.webgiare.org/softpro.jpg";
+$eb_all_themes_support["softpro"]["price"] = 800000;
+//
+$eb_all_themes_support["softpro"]["top"][] = "softpro-top2.php";
+$eb_all_themes_support["softpro"]["top"][] = "softpro-top3.php";
+$eb_all_themes_support["softpro"]["top"][] = "top_widget.php";
+//
+$eb_all_themes_support["softpro"]["footer"][] = "footer_widget.php";
+$eb_all_themes_support["softpro"]["footer"][] = "erawatch-footer2.php.php";
+$eb_all_themes_support["softpro"]["footer"][] = "echbaythree-footer1.php";
+//
+$eb_all_themes_support["softpro"]["home"][] = "home_widget.php";
+//
+$eb_all_themes_support["softpro"]["cf_threaddetails_include_file"] = "softpro-threaddetails.html";
+//
+$eb_all_themes_support["softpro"]["cf_threadnode_include_file"] = "vietnamontrails-threadnode.html";
