f12a23203d843da0c38165bf25396dc8206abd61
itvn9online <itvn9online@gmail.com>
2017-10-14 20:14:19 +0700

    hoang nam

---
+++ themes/all/hoangnam.php
@@ -0,0 +1,34 @@
+<?php 
+/*
+* Save from Sat, 14 Oct 2017 15:48:43 +0700
+* By itvn9online@yahoo.com #1
+* From IP: 127.0.0.1
+* On domain: localhost:8888
+* User agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36
+*/
+$eb_all_themes_support["hoangnam"] = array();
+$eb_all_themes_support["hoangnam"]["name"] = "hoangnam";
+$eb_all_themes_support["hoangnam"]["screenshot"] = "https://img1.webgiare.org/hoangnam.jpg";
+$eb_all_themes_support["hoangnam"]["author"] = "";
+$eb_all_themes_support["hoangnam"]["demo"] = "";
+$eb_all_themes_support["hoangnam"]["price"] = 2000000;
+//
+$eb_all_themes_support["hoangnam"]["top"][] = "hoangnam-top1.php";
+$eb_all_themes_support["hoangnam"]["top"][] = "hoangnam-top2.php";
+$eb_all_themes_support["hoangnam"]["top"][] = "top_widget.php";
+$eb_all_themes_support["hoangnam"]["top"][] = "breadcrumb-top1.php";
+//
+$eb_all_themes_support["hoangnam"]["footer"][] = "footer_widget.php";
+$eb_all_themes_support["hoangnam"]["footer"][] = "hoangnam-footer1.php";
+$eb_all_themes_support["hoangnam"]["footer"][] = "echbaytwo-footer3.php";
+//
+$eb_all_themes_support["hoangnam"]["home"][] = "hoangnam-home1.php";
+$eb_all_themes_support["hoangnam"]["home"][] = "hoangnam-home2.php";
+$eb_all_themes_support["hoangnam"]["home"][] = "hoangnam-home3.php";
+$eb_all_themes_support["hoangnam"]["home"][] = "hoangnam-home4.php";
+$eb_all_themes_support["hoangnam"]["home"][] = "hoangnam-home5.php";
+$eb_all_themes_support["hoangnam"]["home"][] = "home_widget.php";
+//
+$eb_all_themes_support["hoangnam"]["threaddetails"][] = "hoangnam-threaddetails.html";
+//
+$eb_all_themes_support["hoangnam"]["threadnode"][] = "hoangnam-threadnode.html";
