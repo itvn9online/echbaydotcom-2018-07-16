b7879e861f1b4f708f74347661e8a73cb0a20d99
itvn9online <itvn9online@gmail.com>
2017-11-02 11:10:55 +0700

    mobile for echbaythree

--- themes/footer/echbaythree-footer1.php
+++ themes/footer/echbaythree-footer1.php
@@ -2,8 +2,10 @@
 	<div class="<?php echo $__cf_row['cf_footer_class_style']; ?>">
 		<div class="echbaythree-footer1 cf">
 			<div class="lf f75 cf fullsize-if-mobile footer-menu">
-				<div class="lf f40 fullsize-if-mobile"><?php echo EBE_echbay_footer_menu(); ?></div>
-				<div class="lf f40 fullsize-if-mobile"><?php echo EBE_echbay_footer_menu(); ?></div>
+				<div class="lf f40 fullsize-if-mobile"><?php echo EBE_echbay_footer_menu(); ?><br>
+				</div>
+				<div class="lf f40 fullsize-if-mobile"><?php echo EBE_echbay_footer_menu(); ?><br>
+				</div>
 			</div>
 			<div class="lf f25 fullsize-if-mobile">
 				<div><?php echo EBE_get_lang('joinus'); ?></div>
