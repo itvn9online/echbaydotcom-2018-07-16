96b49768e70e4413f0e267787180aab4e33027d5
itvn9online <itvn9online@gmail.com>
2017-11-06 13:36:42 +0700

    wgr-load-js-sub-category

--- echbay/global_html.php
+++ echbay/global_html.php
@@ -60,9 +60,21 @@
 				</div>
 			</li>
 			<li class="cf">
+				<div class="lf f80"><i class="fa fa-circle-o-notch"></i> JS Sub Category <em class="small">(Tạo menu Danh mục sản phẩm bằng Javascript <strong>dùng để làm sub-menu</strong>)</em></div>
+				<div class="lf f20 text-center">
+					<button data-link="#" data-text="Tên menu" data-css="wgr-load-js-sub-category" type="button" class="cur click-to-add-custom-link"><i class="fa fa-plus"></i></button>
+				</div>
+			</li>
+			<li class="cf">
 				<div class="lf f80"><i class="fa fa-circle-o-notch"></i> JS Blogs <em class="small">(Tạo menu Danh mục Tin tức bằng Javascript)</em></div>
 				<div class="lf f20 text-center">
 					<button data-link="#" data-text="Tên menu" data-css="wgr-load-js-blogs" type="button" class="cur click-to-add-custom-link"><i class="fa fa-plus"></i></button>
+				</div>
+			</li>
+			<li class="cf">
+				<div class="lf f80"><i class="fa fa-circle-o-notch"></i> JS Sub Blogs <em class="small">(Tạo menu Danh mục Tin tức bằng Javascript <strong>dùng để làm sub-menu</strong>)</em></div>
+				<div class="lf f20 text-center">
+					<button data-link="#" data-text="Tên menu" data-css="wgr-load-js-sub-blogs" type="button" class="cur click-to-add-custom-link"><i class="fa fa-plus"></i></button>
 				</div>
 			</li>
 			<li class="cf">
