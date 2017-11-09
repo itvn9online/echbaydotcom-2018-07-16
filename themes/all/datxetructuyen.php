badcaa82594c39663ac6bc0b2934a44249e78e29
itvn9online <itvn9online@gmail.com>
2017-11-09 10:20:49 +0700

    dat xe truc tuyen

---
+++ themes/all/datxetructuyen.php
@@ -0,0 +1,31 @@
+<?php 
+/*
+* Save from Thu, 09 Nov 2017 10:19:29 +0700
+* By itvn9online@yahoo.com #1
+* From IP: 127.0.0.1
+* On domain: localhost:8888
+* User agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36
+*/
+$eb_all_themes_support["datxetructuyen"] = array();
+$eb_all_themes_support["datxetructuyen"]["name"] = "datxetructuyen";
+$eb_all_themes_support["datxetructuyen"]["screenshot"] = "https://img1.webgiare.org/datxetructuyen.jpg";
+$eb_all_themes_support["datxetructuyen"]["author"] = "";
+$eb_all_themes_support["datxetructuyen"]["demo"] = "";
+$eb_all_themes_support["datxetructuyen"]["price"] = 2000000;
+//
+$eb_all_themes_support["datxetructuyen"]["top"][] = "datxetructuyen-top1.php";
+$eb_all_themes_support["datxetructuyen"]["top"][] = "top_widget.php";
+$eb_all_themes_support["datxetructuyen"]["top"][] = "bigbanner-top1.php";
+$eb_all_themes_support["datxetructuyen"]["top"][] = "breadcrumb-top1.php";
+//
+$eb_all_themes_support["datxetructuyen"]["footer"][] = "footer_widget.php";
+$eb_all_themes_support["datxetructuyen"]["footer"][] = "datxetructuyen-footer1.php";
+$eb_all_themes_support["datxetructuyen"]["footer"][] = "echbaytwo-footer3.php";
+//
+$eb_all_themes_support["datxetructuyen"]["home"][] = "datxetructuyen-home1.php";
+$eb_all_themes_support["datxetructuyen"]["home"][] = "datxetructuyen-home2.php";
+$eb_all_themes_support["datxetructuyen"]["home"][] = "home_widget.php";
+//
+$eb_all_themes_support["datxetructuyen"]["threaddetails"][] = "datxetructuyen-threaddetails.html";
+//
+$eb_all_themes_support["datxetructuyen"]["threadnode"][] = "datxetructuyen-threadnode.html";
