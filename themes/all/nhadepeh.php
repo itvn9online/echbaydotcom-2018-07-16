ebd88b7caeab0273adb13656a342abd7367f6479
itvn9online <itvn9online@gmail.com>
2017-11-21 21:01:59 +0700

    nha dep eh

---
+++ themes/all/nhadepeh.php
@@ -0,0 +1,37 @@
+<?php 
+/*
+* Save from Tue, 21 Nov 2017 20:07:47 +0700
+* By itvn9online@yahoo.com #1
+* From IP: 127.0.0.1
+* On domain: localhost:8888
+* User agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36
+*/
+$eb_all_themes_support["nhadepeh"] = array();
+$eb_all_themes_support["nhadepeh"]["name"] = "nhadepeh";
+$eb_all_themes_support["nhadepeh"]["screenshot"] = "https://img1.webgiare.org/nhadepeh.jpg";
+$eb_all_themes_support["nhadepeh"]["author"] = "";
+$eb_all_themes_support["nhadepeh"]["demo"] = "";
+$eb_all_themes_support["nhadepeh"]["price"] = 2000000;
+//
+$eb_all_themes_support["nhadepeh"]["top"][] = "nhadepeh-top1.php";
+$eb_all_themes_support["nhadepeh"]["top"][] = "top_widget.php";
+// Banner tràn màn hình 
+$eb_all_themes_support["nhadepeh"]["top"][] = "bigbanner-top1.php";
+// Banner bo gọn trong khung 
+//$eb_all_themes_support["nhadepeh"]["top"][] = "bigbannerFixed-top1.php";
+// breadcrumb tràn màn hình 
+$eb_all_themes_support["nhadepeh"]["top"][] = "breadcrumb-top1.php";
+// breadcrumb gọn trong khung 
+//$eb_all_themes_support["nhadepeh"]["top"][] = "breadcrumb2-top1.php";
+//
+$eb_all_themes_support["nhadepeh"]["footer"][] = "footer_widget.php";
+$eb_all_themes_support["nhadepeh"]["footer"][] = "nhadepeh-footer1.php";
+$eb_all_themes_support["nhadepeh"]["footer"][] = "echbaytwo-footer3.php";
+//
+$eb_all_themes_support["nhadepeh"]["home"][] = "nhadepeh-home1.php";
+$eb_all_themes_support["nhadepeh"]["home"][] = "nhadepeh-home2.php";
+$eb_all_themes_support["nhadepeh"]["home"][] = "home_widget.php";
+//
+$eb_all_themes_support["nhadepeh"]["threaddetails"][] = "nhadepeh-threaddetails.html";
+//
+$eb_all_themes_support["nhadepeh"]["threadnode"][] = "nhadepeh-threadnode.html";
