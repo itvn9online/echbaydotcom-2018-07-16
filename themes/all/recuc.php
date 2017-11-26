1a3b38f091c4ae7a09d1b7efbc2d56360b944d9a
itvn9online <itvn9online@gmail.com>
2017-11-26 07:51:08 +0700

    re cuc theme

---
+++ themes/all/recuc.php
@@ -0,0 +1,37 @@
+<?php 
+/*
+* Save from Sun, 26 Nov 2017 01:21:01 +0700
+* By itvn9online@yahoo.com #1
+* From IP: 127.0.0.1
+* On domain: localhost:8888
+* User agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36
+*/
+$eb_all_themes_support["recuc"] = array();
+$eb_all_themes_support["recuc"]["name"] = "recuc";
+$eb_all_themes_support["recuc"]["screenshot"] = "https://img1.webgiare.org/recuc.jpg";
+$eb_all_themes_support["recuc"]["author"] = "";
+$eb_all_themes_support["recuc"]["demo"] = "";
+$eb_all_themes_support["recuc"]["price"] = 2000000;
+//
+$eb_all_themes_support["recuc"]["top"][] = "recuc-top1.php";
+$eb_all_themes_support["recuc"]["top"][] = "top_widget.php";
+// Banner tràn màn hình 
+$eb_all_themes_support["recuc"]["top"][] = "bigbanner-top1.php";
+// Banner bo gọn trong khung 
+//$eb_all_themes_support["recuc"]["top"][] = "bigbannerFixed-top1.php";
+// breadcrumb tràn màn hình 
+$eb_all_themes_support["recuc"]["top"][] = "breadcrumb-top1.php";
+// breadcrumb gọn trong khung 
+//$eb_all_themes_support["recuc"]["top"][] = "breadcrumb2-top1.php";
+//
+$eb_all_themes_support["recuc"]["footer"][] = "footer_widget.php";
+$eb_all_themes_support["recuc"]["footer"][] = "recuc-footer1.php";
+$eb_all_themes_support["recuc"]["footer"][] = "echbaytwo-footer3.php";
+//
+$eb_all_themes_support["recuc"]["home"][] = "recuc-home1.php";
+$eb_all_themes_support["recuc"]["home"][] = "recuc-home2.php";
+$eb_all_themes_support["recuc"]["home"][] = "home_widget.php";
+//
+$eb_all_themes_support["recuc"]["threaddetails"][] = "recuc-threaddetails.html";
+//
+$eb_all_themes_support["recuc"]["threadnode"][] = "recuc-threadnode.html";
