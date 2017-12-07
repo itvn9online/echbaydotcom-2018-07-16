120837e9936ded8d056a3ef16daa1db3fd8dd093
itvn9online <itvn9online@gmail.com>
2017-12-08 03:14:43 +0700

    vien tham my korea

---
+++ themes/all/vienthammykorea.php
@@ -0,0 +1,36 @@
+<?php 
+/*
+* Save from Tue, 05 Dec 2017 21:25:00 +0700
+* By itvn9online@yahoo.com #1
+* From IP: 127.0.0.1
+* On domain: localhost:8888
+* User agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36
+*/
+$eb_all_themes_support["vienthammykorea"] = array();
+$eb_all_themes_support["vienthammykorea"]["name"] = "vienthammykorea";
+$eb_all_themes_support["vienthammykorea"]["screenshot"] = "https://img1.webgiare.org/vienthammykorea.jpg";
+$eb_all_themes_support["vienthammykorea"]["author"] = "";
+$eb_all_themes_support["vienthammykorea"]["demo"] = "";
+$eb_all_themes_support["vienthammykorea"]["price"] = 2000000;
+//
+$eb_all_themes_support["vienthammykorea"]["top"][] = "vienthammykorea-top1.php";
+$eb_all_themes_support["vienthammykorea"]["top"][] = "top_widget.php";
+// Banner tràn màn hình 
+$eb_all_themes_support["vienthammykorea"]["top"][] = "bigbanner-top1.php";
+// Banner bo gọn trong khung 
+//$eb_all_themes_support["vienthammykorea"]["top"][] = "bigbannerFixed-top1.php";
+// breadcrumb tràn màn hình 
+$eb_all_themes_support["vienthammykorea"]["top"][] = "breadcrumb-top1.php";
+// breadcrumb gọn trong khung 
+//$eb_all_themes_support["vienthammykorea"]["top"][] = "breadcrumb2-top1.php";
+//
+$eb_all_themes_support["vienthammykorea"]["footer"][] = "footer_widget.php";
+$eb_all_themes_support["vienthammykorea"]["footer"][] = "vienthammykorea-footer1.php";
+$eb_all_themes_support["vienthammykorea"]["footer"][] = "echbaytwo-footer3.php";
+//
+$eb_all_themes_support["vienthammykorea"]["home"][] = "vienthammykorea-home1.php";
+$eb_all_themes_support["vienthammykorea"]["home"][] = "home_widget.php";
+//
+$eb_all_themes_support["vienthammykorea"]["threaddetails"][] = "vienthammykorea-threaddetails.html";
+//
+$eb_all_themes_support["vienthammykorea"]["threadnode"][] = "vienthammykorea-threadnode.html";
