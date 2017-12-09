e0309697d8b72a0da06720278d40d47c3b4ba6a8
itvn9online <itvn9online@gmail.com>
2017-12-09 08:11:09 +0700

    okia theme

---
+++ themes/all/okiaglobal.php
@@ -0,0 +1,36 @@
+<?php 
+/*
+* Save from Sat, 09 Dec 2017 08:07:08 +0700
+* By itvn9online@yahoo.com #1
+* From IP: 127.0.0.1
+* On domain: localhost:8888
+* User agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36
+*/
+$eb_all_themes_support["okiaglobal"] = array();
+$eb_all_themes_support["okiaglobal"]["name"] = "okiaglobal";
+$eb_all_themes_support["okiaglobal"]["screenshot"] = "https://img1.webgiare.org/okiaglobal.jpg";
+$eb_all_themes_support["okiaglobal"]["author"] = "";
+$eb_all_themes_support["okiaglobal"]["demo"] = "";
+$eb_all_themes_support["okiaglobal"]["price"] = 2000000;
+//
+$eb_all_themes_support["okiaglobal"]["top"][] = "okiaglobal-top1.php";
+$eb_all_themes_support["okiaglobal"]["top"][] = "top_widget.php";
+// Banner tràn màn hình 
+$eb_all_themes_support["okiaglobal"]["top"][] = "bigbanner-top1.php";
+// Banner bo gọn trong khung 
+//$eb_all_themes_support["okiaglobal"]["top"][] = "bigbannerFixed-top1.php";
+// breadcrumb tràn màn hình 
+$eb_all_themes_support["okiaglobal"]["top"][] = "breadcrumb-top1.php";
+// breadcrumb gọn trong khung 
+//$eb_all_themes_support["okiaglobal"]["top"][] = "breadcrumb2-top1.php";
+//
+$eb_all_themes_support["okiaglobal"]["footer"][] = "footer_widget.php";
+$eb_all_themes_support["okiaglobal"]["footer"][] = "okiaglobal-footer1.php";
+$eb_all_themes_support["okiaglobal"]["footer"][] = "echbaytwo-footer3.php";
+//
+$eb_all_themes_support["okiaglobal"]["home"][] = "okiaglobal-home1.php";
+$eb_all_themes_support["okiaglobal"]["home"][] = "home_widget.php";
+//
+$eb_all_themes_support["okiaglobal"]["threaddetails"][] = "okiaglobal-threaddetails.html";
+//
+$eb_all_themes_support["okiaglobal"]["threadnode"][] = "okiaglobal-threadnode.html";
