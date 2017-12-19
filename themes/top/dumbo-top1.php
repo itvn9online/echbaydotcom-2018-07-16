8428fcafbe666a3ffbdbe27fb075b5b6ef1afc13
itvn9online <itvn9online@gmail.com>
2017-12-19 12:16:42 +0700

    dem bao phat

--- themes/top/dumbo-top1.php
+++
@@ -1,5 +0,0 @@
-<div id="dumbo-top1" class="default-bg hide-if-mobile">
-	<div class="<?php echo $__cf_row['cf_top_class_style']; ?>">
-		<div class="dumbo-top1"><?php echo EBE_get_html_profile(); ?></div>
-	</div>
-</div>
