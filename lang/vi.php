953e70a9bc8aabf6c99b72480fbe419806edffcc
itvn9online <itvn9online@gmail.com>
2018-02-06 15:52:34 +0700

    tag_blog_excerpt

--- lang/vi.php
+++ lang/vi.php
@@ -188,7 +188,7 @@ $___eb_lang[eb_key_for_site_lang . 'bigbanner_num'] = '5';
 $eb_type_lang[eb_key_for_site_lang . 'bigbanner_num'] = 'number';
 
 // thẻ H2 cho phần chi tiết tin tức
-$eb_type_lang[eb_key_for_site_lang . 'tag_blog_excerpt'] = 'h2';
+$___eb_lang[eb_key_for_site_lang . 'tag_blog_excerpt'] = 'h2';
 
 
 // phần ngôn ngữ riêng, để sử dụng cho các câu từ mà một số website sẽ dùng
