4e7c72b3f0b9595f9b4996e902fab110a62ede27
itvn9online <itvn9online@gmail.com>
2018-03-01 04:43:16 +0700

    class for page speed

--- echbay/dashboard.php
+++ echbay/dashboard.php
@@ -233,4 +233,4 @@ else {
 
 
 ?>
-<p class="greencolor"><i class="fa fa-lightbulb-o orgcolor"></i> Xem và cải thiện tốc độ cho website bằng công cụ <a href="https://developers.google.com/speed/pagespeed/insights/?url=<?php echo urlencode( web_link ); ?>&tab=mobile" target="_blank" rel="nofollow">PageSpeed Insights</a> của Google.</p>
+<p class="greencolor medium"><i class="fa fa-lightbulb-o orgcolor"></i> Thường xuyên xem và cải thiện tốc độ cho website bằng công cụ <a href="https://developers.google.com/speed/pagespeed/insights/?url=<?php echo urlencode( web_link ); ?>&tab=mobile" target="_blank" rel="nofollow">PageSpeed Insights</a> của Google để tối ưu tốc độ tải trang cho website của bạn.</p>
