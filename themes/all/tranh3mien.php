7756576a859a10fef220e4b90b9f6042b39c1b61
itvn9online <itvn9online@gmail.com>
2018-03-29 15:06:54 +0700

    tranh 3 mien

--- themes/all/tranh3mien.php
+++ themes/all/tranh3mien.php
@@ -1,20 +1,20 @@
 <?php 
 /*
-* Save from Wed, 21 Mar 2018 08:54:14 +0700
+* Save from Wed, 28 Mar 2018 11:47:26 +0700
 * By itvn9online@yahoo.com #1
 * From IP: 127.0.0.1
 * On domain: localhost:8888
-* User agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.162 Safari/537.36
+* User agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36
 */
-$eb_all_themes_name = 'smartmedia';
+$eb_all_themes_name = 'tranh3mien';
 $eb_all_themes_support[$eb_all_themes_name] = array();
 $eb_all_themes_support[$eb_all_themes_name]["name"] = $eb_all_themes_name;
-$eb_all_themes_support[$eb_all_themes_name]["screenshot"] = "https://img1.webgiare.org/smartmedia.jpg";
+$eb_all_themes_support[$eb_all_themes_name]["screenshot"] = "https://img1.webgiare.org/tranh3mien.jpg";
 $eb_all_themes_support[$eb_all_themes_name]["author"] = "";
 $eb_all_themes_support[$eb_all_themes_name]["demo"] = "";
 $eb_all_themes_support[$eb_all_themes_name]["price"] = 2000000;
 //
-$eb_all_themes_support[$eb_all_themes_name]["top"][] = "smartmedia-top1.php";
+$eb_all_themes_support[$eb_all_themes_name]["top"][] = "tranh3mien-top1.php";
 $eb_all_themes_support[$eb_all_themes_name]["top"][] = "top_widget.php";
 // Banner tràn màn hình
 $eb_all_themes_support[$eb_all_themes_name]["top"][] = "bigbanner-top1.php";
@@ -26,12 +26,12 @@ $eb_all_themes_support[$eb_all_themes_name]["top"][] = "bigbanner-top1.php";
 $eb_all_themes_support[$eb_all_themes_name]["top"][] = "breadcrumb2-top1.php";
 //
 $eb_all_themes_support[$eb_all_themes_name]["footer"][] = "footer_widget.php";
-$eb_all_themes_support[$eb_all_themes_name]["footer"][] = "smartmedia-footer1.php";
-//$eb_all_themes_support[$eb_all_themes_name]["footer"][] = "echbaytwo-footer3.php";
+$eb_all_themes_support[$eb_all_themes_name]["footer"][] = "tranh3mien-footer1.php";
+$eb_all_themes_support[$eb_all_themes_name]["footer"][] = "echbaytwo-footer3.php";
 //
-//$eb_all_themes_support[$eb_all_themes_name]["home"][] = "smartmedia-home1.php";
+$eb_all_themes_support[$eb_all_themes_name]["home"][] = "tranh3mien-home1.php";
 $eb_all_themes_support[$eb_all_themes_name]["home"][] = "home_widget.php";
 //
-$eb_all_themes_support[$eb_all_themes_name]["threaddetails"][] = "smartmedia-threaddetails.html";
+$eb_all_themes_support[$eb_all_themes_name]["threaddetails"][] = "tranh3mien-threaddetails.html";
 //
-$eb_all_themes_support[$eb_all_themes_name]["threadnode"][] = "smartmedia-threadnode.html";
+$eb_all_themes_support[$eb_all_themes_name]["threadnode"][] = "tranh3mien-threadnode.html";
