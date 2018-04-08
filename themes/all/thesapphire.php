e707198cca01b16aa3551bd39ab36b635bc2899e
itvn9online <itvn9online@gmail.com>
2018-04-09 01:52:24 +0700

    The Sapphire

---
+++ themes/all/thesapphire.php
@@ -0,0 +1,37 @@
+<?php 
+/*
+* Save from Mon, 09 Apr 2018 01:41:16 +0700
+* By itvn9online@yahoo.com #1
+* From IP: 127.0.0.1
+* On domain: localhost:8888
+* User agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36
+*/
+$eb_all_themes_name = 'thesapphire';
+$eb_all_themes_support[$eb_all_themes_name] = array();
+$eb_all_themes_support[$eb_all_themes_name]["name"] = $eb_all_themes_name;
+$eb_all_themes_support[$eb_all_themes_name]["screenshot"] = "https://img1.webgiare.org/thesapphire.jpg";
+$eb_all_themes_support[$eb_all_themes_name]["author"] = "";
+$eb_all_themes_support[$eb_all_themes_name]["demo"] = "";
+$eb_all_themes_support[$eb_all_themes_name]["price"] = 2000000;
+//
+$eb_all_themes_support[$eb_all_themes_name]["top"][] = "thesapphire-top1.php";
+$eb_all_themes_support[$eb_all_themes_name]["top"][] = "top_widget.php";
+// Banner tràn màn hình
+$eb_all_themes_support[$eb_all_themes_name]["top"][] = "bigbanner-top1.php";
+// Banner bo gọn trong khung
+//$eb_all_themes_support[$eb_all_themes_name]["top"][] = "bigbannerFixed-top1.php";
+// breadcrumb tràn màn hình
+//$eb_all_themes_support[$eb_all_themes_name]["top"][] = "breadcrumb-top1.php";
+// breadcrumb gọn trong khung
+$eb_all_themes_support[$eb_all_themes_name]["top"][] = "breadcrumb2-top1.php";
+//
+$eb_all_themes_support[$eb_all_themes_name]["footer"][] = "footer_widget.php";
+$eb_all_themes_support[$eb_all_themes_name]["footer"][] = "thesapphire-footer1.php";
+$eb_all_themes_support[$eb_all_themes_name]["footer"][] = "echbaytwo-footer3.php";
+//
+$eb_all_themes_support[$eb_all_themes_name]["home"][] = "thesapphire-home1.php";
+$eb_all_themes_support[$eb_all_themes_name]["home"][] = "home_widget.php";
+//
+$eb_all_themes_support[$eb_all_themes_name]["threaddetails"][] = "thesapphire-threaddetails.html";
+//
+$eb_all_themes_support[$eb_all_themes_name]["threadnode"][] = "thesapphire-threadnode.html";
