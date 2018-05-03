29c4714eeccbae14f59f64e5e0b181f4b4b938c7
itvn9online <itvn9online@gmail.com>
2018-05-04 05:32:27 +0700

    a ha long

---
+++ themes/all/ahalong.php
@@ -0,0 +1,37 @@
+<?php 
+/*
+* Save from Fri, 04 May 2018 05:27:01 +0700
+* By itvn9online@yahoo.com #1
+* From IP: 127.0.0.1
+* On domain: localhost:8888
+* User agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36
+*/
+$eb_all_themes_name = 'ahalong';
+$eb_all_themes_support[$eb_all_themes_name] = array();
+$eb_all_themes_support[$eb_all_themes_name]["name"] = $eb_all_themes_name;
+$eb_all_themes_support[$eb_all_themes_name]["screenshot"] = "https://img1.webgiare.org/ahalong.jpg";
+$eb_all_themes_support[$eb_all_themes_name]["author"] = "";
+$eb_all_themes_support[$eb_all_themes_name]["demo"] = "";
+$eb_all_themes_support[$eb_all_themes_name]["price"] = 2000000;
+//
+$eb_all_themes_support[$eb_all_themes_name]["top"][] = "ahalong-top1.php";
+$eb_all_themes_support[$eb_all_themes_name]["top"][] = "top_widget.php";
+// Banner tràn màn hình
+$eb_all_themes_support[$eb_all_themes_name]["top"][] = "bigbanner-top1.php";
+// Banner bo gọn trong khung
+//$eb_all_themes_support[$eb_all_themes_name]["top"][] = "bigbannerFixed-top1.php";
+// breadcrumb tràn màn hình
+//$eb_all_themes_support[$eb_all_themes_name]["top"][] = "breadcrumb-top1.php";
+// breadcrumb gọn trong khung
+$eb_all_themes_support[$eb_all_themes_name]["top"][] = "breadcrumb2-top1.php";
+//
+$eb_all_themes_support[$eb_all_themes_name]["footer"][] = "footer_widget.php";
+$eb_all_themes_support[$eb_all_themes_name]["footer"][] = "ahalong-footer1.php";
+$eb_all_themes_support[$eb_all_themes_name]["footer"][] = "echbaytwo-footer3.php";
+//
+$eb_all_themes_support[$eb_all_themes_name]["home"][] = "ahalong-home1.php";
+$eb_all_themes_support[$eb_all_themes_name]["home"][] = "home_widget.php";
+//
+$eb_all_themes_support[$eb_all_themes_name]["threaddetails"][] = "ahalong-threaddetails.html";
+//
+$eb_all_themes_support[$eb_all_themes_name]["threadnode"][] = "ahalong-threadnode.html";
