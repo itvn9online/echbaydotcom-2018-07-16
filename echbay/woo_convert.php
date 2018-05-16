af021a8e4b4284484dbc18c8db4bd8383e7d90e1
itvn9online <itvn9online@gmail.com>
2018-05-16 20:44:06 +0700

    woo convert

--- echbay/woo_convert.php
+++ echbay/woo_convert.php
@@ -1,5 +1,22 @@
 <?php
 
+$convert_lnk = admin_link . 'admin.php?page=eb-coder&tab=woo_convert';
+
+?>
+
+<h2>Convert các dữ liệu trong post meta sang Commerce hoặc ngược lại</h2>
+<br>
+<ol>
+	<li><a href="<?php echo $convert_lnk; ?>">Woo to WebGiaRe</a></li>
+	<li><a href="<?php echo $convert_lnk; ?>&mt=wgr">WebGiaRe to Woo</a></li>
+	<li><a href="<?php echo $convert_lnk; ?>&mt=yoast">WebGiaRe to Yoast SEO</a></li>
+	<li><a href="<?php echo $convert_lnk; ?>&mt=yoast_wgr">Yoast SEO to WebGiaRe</a></li>
+	<li><a href="<?php echo $convert_lnk; ?>&mt=ultimate">WebGiaRe to SEO Ultimate</a></li>
+	<li><a href="<?php echo $convert_lnk; ?>&mt=ultimatewgr_wgr">SEO Ultimate to WebGiaRe</a></li>
+</ol>
+<br>
+<?php
+
 
 
 // chuyển tên các key trong woo sang echbay
@@ -16,13 +33,54 @@ function WGR_woo_convert ( $old_key, $new_key ) {
 //	print_r( $sql );
 	
 	foreach ( $sql as $v ) {
-		WGR_update_meta_post( $v->post_id, $new_key, $v->meta_value );
+		// copy sang giá trị mới
+//		if ( $v->meta_value != 0 ) {
+			WGR_update_meta_post( $v->post_id, $new_key, $v->meta_value );
+//		}
+		
+		// xóa cái cũ
+		delete_post_meta( $v->post_id, $old_key );
 	}
+	
+	
+	// xóa các cột dữ liệu trống
+	_eb_q("DELETE
+	FROM
+		`" . wp_postmeta . "`
+	WHERE
+		meta_key = '" . $old_key . "'
+		AND meta_value = ''", 0);
+	
+	
+	//
+	echo '<div class="medium18"><strong>' . count( $sql ) . ' ' . $old_key . '</strong> has been convert to: <strong>' . $new_key . '</strong></div><br>';
+	
+	//
+	return true;
 }
 
+
+
 //
-WGR_woo_convert( '_sku', '_eb_product_sku' );
-WGR_woo_convert( '_regular_price', '_eb_product_oldprice' );
-WGR_woo_convert( '_sale_price', '_eb_product_price' );
+$mt = '';
+if ( isset( $_GET['mt'] ) ) {
+	$mt = trim( $_GET['mt'] );
+}
+//echo $mt . '<br>';
+
+// từ wgr sang woo
+if ( $mt == 'wgr' ) {
+	WGR_woo_convert( '_eb_product_sku', '_sku' );
+	WGR_woo_convert( '_eb_product_oldprice', '_regular_price' );
+	WGR_woo_convert( '_eb_product_price', '_sale_price' );
+}
+// từ woo sang wgr
+else if ( $mt == '' ) {
+	WGR_woo_convert( '_sku', '_eb_product_sku' );
+	WGR_woo_convert( '_regular_price', '_eb_product_oldprice' );
+	WGR_woo_convert( '_sale_price', '_eb_product_price' );
+}
+
+
 
 
