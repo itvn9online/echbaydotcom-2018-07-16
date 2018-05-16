57585e114f84ed123b604c163d05a33f1553e35b
itvn9online <itvn9online@gmail.com>
2018-05-16 20:55:09 +0700

    edit menu in woo convert

--- echbay/woo_convert.php
+++ echbay/woo_convert.php
@@ -4,15 +4,15 @@ $convert_lnk = admin_link . 'admin.php?page=eb-coder&tab=woo_convert';
 
 ?>
 
-<h2>Convert các dữ liệu trong post meta sang Commerce hoặc ngược lại</h2>
+<h2>Convert các dữ liệu trong post meta từ plugin này sang plugin khác (bấm chọn bên dưới):</h2>
 <br>
 <ol>
-	<li><a href="<?php echo $convert_lnk; ?>">Woo to WebGiaRe</a></li>
-	<li><a href="<?php echo $convert_lnk; ?>&mt=wgr">WebGiaRe to Woo</a></li>
-	<li><a href="<?php echo $convert_lnk; ?>&mt=yoast">WebGiaRe to Yoast SEO</a></li>
-	<li><a href="<?php echo $convert_lnk; ?>&mt=yoast_wgr">Yoast SEO to WebGiaRe</a></li>
-	<li><a href="<?php echo $convert_lnk; ?>&mt=ultimate">WebGiaRe to SEO Ultimate</a></li>
-	<li><a href="<?php echo $convert_lnk; ?>&mt=ultimatewgr_wgr">SEO Ultimate to WebGiaRe</a></li>
+	<li><a href="<?php echo $convert_lnk; ?>&mt=woo">Woo-commerce to WebGiaRe</a></li>
+	<li><a href="<?php echo $convert_lnk; ?>&mt=wgr">WebGiaRe to Woo-commerce</a></li>
+	<li><a href="<?php echo $convert_lnk; ?>&mt=yoast">Yoast SEO to WebGiaRe</a></li>
+	<li><a href="<?php echo $convert_lnk; ?>&mt=wgr_yoast">WebGiaRe to Yoast SEO</a></li>
+	<li><a href="<?php echo $convert_lnk; ?>&mt=ultimatewgr">SEO Ultimate to WebGiaRe</a></li>
+	<li><a href="<?php echo $convert_lnk; ?>&mt=wgr_ultimate">WebGiaRe to SEO Ultimate</a></li>
 </ol>
 <br>
 <?php
@@ -75,7 +75,7 @@ if ( $mt == 'wgr' ) {
 	WGR_woo_convert( '_eb_product_price', '_sale_price' );
 }
 // từ woo sang wgr
-else if ( $mt == '' ) {
+else if ( $mt == 'woo' ) {
 	WGR_woo_convert( '_sku', '_eb_product_sku' );
 	WGR_woo_convert( '_regular_price', '_eb_product_oldprice' );
 	WGR_woo_convert( '_sale_price', '_eb_product_price' );
