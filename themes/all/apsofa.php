9746f45fffd85a0e870afea8a702caba15c91d59
itvn9online <itvn9online@gmail.com>
2018-05-22 13:54:58 +0700

    ap sofa

--- themes/all/apsofa.php
+++ themes/all/apsofa.php
@@ -32,7 +32,7 @@ $eb_all_themes_support[$eb_all_themes_name]["footer"][] = "apsofa-footer1.php";
 $eb_all_themes_support[$eb_all_themes_name]["footer"][] = "echbaytwo-footer3.php";
 //
 //$eb_all_themes_support[$eb_all_themes_name]["home"][] = "apsofa-home1.php";
-//$eb_all_themes_support[$eb_all_themes_name]["home"][] = "home_widget.php";
+$eb_all_themes_support[$eb_all_themes_name]["home"][] = "home_widget.php";
 //
 $eb_all_themes_support[$eb_all_themes_name]["threaddetails"][] = "apsofa-threaddetails.html";
 //
