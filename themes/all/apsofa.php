bae308fc468ecdeb4a0edcdb6351866e912f2a35
itvn9online <itvn9online@gmail.com>
2018-05-22 14:34:54 +0700

    price for theme

--- themes/all/apsofa.php
+++ themes/all/apsofa.php
@@ -12,7 +12,7 @@ $eb_all_themes_support[$eb_all_themes_name]["name"] = $eb_all_themes_name;
 $eb_all_themes_support[$eb_all_themes_name]["screenshot"] = "https://img1.webgiare.org/apsofa.jpg";
 $eb_all_themes_support[$eb_all_themes_name]["author"] = "";
 $eb_all_themes_support[$eb_all_themes_name]["demo"] = "";
-$eb_all_themes_support[$eb_all_themes_name]["price"] = 2000000;
+$eb_all_themes_support[$eb_all_themes_name]["price"] = 4500000;
 //
 $eb_all_themes_support[$eb_all_themes_name]["top"][] = "apsofa-top1.php";
 $eb_all_themes_support[$eb_all_themes_name]["top"][] = "top_widget.php";
