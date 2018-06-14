37bf4b5f3acceccffcd934844cdd1a81254162b1
itvn9online <itvn9online@gmail.com>
2018-06-14 08:43:13 +0700

    remove motocenter from free version

--- themes/top/motocenter-top3.php
+++
@@ -1,8 +0,0 @@
-<div id="motocenter-top3">
-	<div class="web-top-logo cf <?php echo $__cf_row['cf_top_class_style']; ?>">
-		<div class="lf f25 cf fullsize-if-mobile"><?php echo EBE_get_html_logo(); ?></div>
-		<div class="lf f75 hide-if-mobile">
-			<div class="nav"><?php echo EBE_echbay_top_menu(); ?></div>
-		</div>
-	</div>
-</div>
