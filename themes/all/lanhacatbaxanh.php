ba5ab2835616c0cc7b7cbe9f1009b7ab427c1adb
itvn9online <itvn9online@gmail.com>
2018-06-19 13:53:44 +0700

    home for lan ha

--- themes/all/lanhacatbaxanh.php
+++ themes/all/lanhacatbaxanh.php
@@ -29,7 +29,7 @@ $eb_all_themes_support[$eb_all_themes_name]["footer"][] = "footer_widget.php";
 $eb_all_themes_support[$eb_all_themes_name]["footer"][] = "lanhacatbaxanh-footer1.php";
 $eb_all_themes_support[$eb_all_themes_name]["footer"][] = "echbaytwo-footer3.php";
 //
-$eb_all_themes_support[$eb_all_themes_name]["home"][] = "lanhacatbaxanh-home1.php";
+//$eb_all_themes_support[$eb_all_themes_name]["home"][] = "lanhacatbaxanh-home1.php";
 $eb_all_themes_support[$eb_all_themes_name]["home"][] = "home_widget.php";
 //
 $eb_all_themes_support[$eb_all_themes_name]["threaddetails"][] = "lanhacatbaxanh-threaddetails.html";
