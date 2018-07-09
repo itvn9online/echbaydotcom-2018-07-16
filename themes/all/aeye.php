56a5a63331bd7c5bfaf736f9c63c8c7ff4e5a213
itvn9online <itvn9online@gmail.com>
2018-07-09 10:46:43 +0700

    new theme

---
+++ themes/all/aeye.php
@@ -0,0 +1,37 @@
+<?php 
+/*
+* Save from Mon, 09 Jul 2018 10:40:04 +0700
+* By itvn9online@yahoo.com #1
+* From IP: 127.0.0.1
+* On domain: localhost:8888
+* User agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36
+*/
+$eb_all_themes_name = 'aeye';
+$eb_all_themes_support[$eb_all_themes_name] = array();
+$eb_all_themes_support[$eb_all_themes_name]["name"] = $eb_all_themes_name;
+$eb_all_themes_support[$eb_all_themes_name]["screenshot"] = "https://img1.webgiare.org/aeye.jpg";
+$eb_all_themes_support[$eb_all_themes_name]["author"] = "";
+$eb_all_themes_support[$eb_all_themes_name]["demo"] = "";
+$eb_all_themes_support[$eb_all_themes_name]["price"] = 4500000;
+//
+$eb_all_themes_support[$eb_all_themes_name]["top"][] = "aeye-top1.php";
+$eb_all_themes_support[$eb_all_themes_name]["top"][] = "top_widget.php";
+// Banner tràn màn hình
+$eb_all_themes_support[$eb_all_themes_name]["top"][] = "bigbanner-top1.php";
+// Banner bo gọn trong khung
+//$eb_all_themes_support[$eb_all_themes_name]["top"][] = "bigbannerFixed-top1.php";
+// breadcrumb tràn màn hình
+//$eb_all_themes_support[$eb_all_themes_name]["top"][] = "breadcrumb-top1.php";
+// breadcrumb gọn trong khung
+$eb_all_themes_support[$eb_all_themes_name]["top"][] = "breadcrumb2-top1.php";
+//
+$eb_all_themes_support[$eb_all_themes_name]["footer"][] = "footer_widget.php";
+$eb_all_themes_support[$eb_all_themes_name]["footer"][] = "aeye-footer1.php";
+$eb_all_themes_support[$eb_all_themes_name]["footer"][] = "echbaytwo-footer3.php";
+//
+$eb_all_themes_support[$eb_all_themes_name]["home"][] = "aeye-home1.php";
+$eb_all_themes_support[$eb_all_themes_name]["home"][] = "home_widget.php";
+//
+$eb_all_themes_support[$eb_all_themes_name]["threaddetails"][] = "aeye-threaddetails.html";
+//
+$eb_all_themes_support[$eb_all_themes_name]["threadnode"][] = "aeye-threadnode.html";
