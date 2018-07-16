33bf0d9f70364fb400a5628f93cff8aba39f308d
itvn9online <itvn9online@gmail.com>
2018-07-16 20:49:07 +0700

    WGR_disable_rss_feed

--- plugins/disable-rss-feed.php
+++ plugins/disable-rss-feed.php
@@ -1,17 +1,17 @@
 <?php
 
 
-function itsme_disable_feed() {
+function WGR_disable_rss_feed() {
 	wp_die( __( 'No feed available, please visit the <a href="'. esc_url( home_url( '/' ) ) .'">homepage</a>!' ) );
 }
 
-add_filter('do_feed', 'itsme_disable_feed', 1);
-add_filter('do_feed_rdf', 'itsme_disable_feed', 1);
-add_filter('do_feed_rss', 'itsme_disable_feed', 1);
-add_filter('do_feed_rss2', 'itsme_disable_feed', 1);
-add_filter('do_feed_atom', 'itsme_disable_feed', 1);
-add_filter('do_feed_rss2_comments', 'itsme_disable_feed', 1);
-add_filter('do_feed_atom_comments', 'itsme_disable_feed', 1);
+add_filter('do_feed', 'WGR_disable_rss_feed', 1);
+add_filter('do_feed_rdf', 'WGR_disable_rss_feed', 1);
+add_filter('do_feed_rss', 'WGR_disable_rss_feed', 1);
+add_filter('do_feed_rss2', 'WGR_disable_rss_feed', 1);
+add_filter('do_feed_atom', 'WGR_disable_rss_feed', 1);
+add_filter('do_feed_rss2_comments', 'WGR_disable_rss_feed', 1);
+add_filter('do_feed_atom_comments', 'WGR_disable_rss_feed', 1);
 
 
 
